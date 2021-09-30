43dc13073d02f554768b1b5a1d553fa9455b6657
Samyak Jain <samyak65400@gmail.com>
2021-10-01 01:34:59 +0530

    +1

---
+++ Top Interview Questions/Easy/141. Linked List Cycle.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/linked-list-cycle/
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def hasCycle(head: ListNode) -> bool:
+
+    # Slow and Fast Pointer Approach <3: (https://en.wikipedia.org/wiki/Cycle_detection#Floyd's_tortoise_and_hare)
+
+    slow = fast = head  # at first both pointing at head
+
+    while fast and fast.next:  # while not reached the end of the linked list
+
+        slow = slow.next  # 1 move
+        fast = fast.next.next  # 2 moves
+
+        if fast == slow:  # if they end up on some same node anytime
+            return True  # loop is there
+
+    return False  # else not
