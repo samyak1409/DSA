f6bb9476d56c73ddf24fabcf79d6fd1e77230e7c
Samyak Jain <samyak65400@gmail.com>
2022-01-26 14:40:00 +0530

    removed additional spaces

--- Top Interview Questions/Easy/344. Reverse String.py
+++ Top Interview Questions/Easy/344. Reverse String.py
@@ -13,5 +13,5 @@ def reverseString(s: List[str]) -> None:
 
     # Two Pointer: TC = O(n); SC = O(1)
 
-    for i in range(len(s) // 2):
-        s[i], s[-i - 1] = s[-i - 1], s[i]
+    for i in range(len(s)//2):
+        s[i], s[-i-1] = s[-i-1], s[i]
