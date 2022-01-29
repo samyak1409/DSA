da812e40dee9ce860d560e7fdfeeb922ac3e1009
Samyak Jain <samyak65400@gmail.com>
2022-01-30 02:51:59 +0530

    better approach added

--- Top Interview Questions/Easy/118. Pascal's Triangle.py
+++ Top Interview Questions/Easy/118. Pascal's Triangle.py
@@ -8,20 +8,40 @@ from typing import List
 
 def generate(num_rows: int) -> List[List[int]]:  # TC = O(n^2) = SC
 
-    res = [[1], [1, 1]]  # initialization
+    # 1) Aam Jeevan:
+
+    """
+    ans = [[1], [1, 1]]  # initialization
 
     if num_rows in (1, 2):  # no summation
-        return res[:num_rows]
+        return ans[:num_rows]
 
     # Summation using a nested for loop:
 
     for i in range(1, num_rows-1):  # (num_rows-2) times
 
-        res.append([1])  # (prefix)
+        ans.append([1])  # (prefix)
 
         for j in range(i):  # (1 to num_rows-2) times
-            res[i+1].append(res[i][j]+res[i][j+1])  # mid numbers' calc
+            ans[i+1].append(ans[i][j]+ans[i][j+1])  # mid. numbers' calc
 
-        res[i+1].append(1)  # (suffix)
+        ans[i+1].append(1)  # (suffix)
 
-    return res
+    return ans
+    """
+
+    # 2) Mentos Jeevan: https://leetcode.com/problems/pascals-triangle/discuss/38128/Python-4-lines-short-solution-using-map
+
+    """
+    Explanation: Any row can be constructed using the offset sum of the previous row.
+    
+    Example:
+              1 3 3 1 0 
+            + 0 1 3 3 1
+            = 1 4 6 4 1
+    """
+
+    ans = [[1]]
+    for _ in range(num_rows-1):
+        ans.append(list(map(lambda x, y: x+y, [0]+ans[-1], ans[-1]+[0])))
+    return ans
