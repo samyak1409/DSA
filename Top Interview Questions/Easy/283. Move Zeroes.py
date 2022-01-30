ad9b22e62ff3389d74b3921128e9d9d12866dba2
Samyak Jain <samyak65400@gmail.com>
2022-01-31 04:22:55 +0530

    + approaches + improves

--- Top Interview Questions/Easy/283. Move Zeroes.py
+++ Top Interview Questions/Easy/283. Move Zeroes.py
@@ -19,28 +19,47 @@ def moveZeroes(nums: List[int]) -> None:
         nums.append(0)  # O(1)
     """
 
-    # 2) Efficient (mine): TC = O(n); SC = O(1)
+    # 2) Brute Force: TC = O(n); SC = O(n)
 
-    i = j = 0
-
-    for _ in range(len(nums)-1):  # replacing zeroes with next elements
-        if nums[i] == 0:
-            j += 1
-        else:
-            i += 1
-        nums[i] = nums[i+j]
-
-    print(nums)  # debug 👌
-
-    for i in range(j):  # replacing last j elements with zeroes
+    """
+    store = []  # stores non-zero numbers
+    for num in filter(None, nums):
+        store.append(num)
+    for i in range(len(store)):  # put the non-zero numbers in the front
+        nums[i] = store[i]
+    for i in range(len(nums)-len(store)):  # put the zeros in the back
         nums[-i-1] = 0
+    """
 
-    # 3) Efficient (from "Discuss"): TC = O(n); SC = O(1)
+    # 2.1) Efficient: TC = O(n); SC = O(1)
 
     """
-    zero = 0  # records the position of "0"
+    front = 0
+    for i in range(len(nums)):  # copying the non-zero numbers to the front
+        if nums[i] != 0:  # if num is non-zero
+            nums[front] = nums[i]  # copy num to the front
+            front += 1  # inc front
+    for i in range(len(nums)-front):  # filling the end indices with 0
+        nums[-i-1] = 0
+    """
+
+    # 3) Efficient: TC = O(n); SC = O(1)
+
+    """
+    zeroes = 0
+    for i in range(len(nums)):  # copying the non-zero numbers to the front
+        if nums[i] == 0:
+            zeroes += 1
+        else:  # non-zero
+            nums[i-zeroes] = nums[i]  # copy num to the front
+    for i in range(zeroes):  # filling the end indices with 0
+        nums[-i-1] = 0
+    """
+
+    # 4) Best (in terms of no. of ops): TC = O(n); SC = O(1)
+
+    zero = 0  # records the index of 0
     for i in range(len(nums)):
-        if nums[i] != 0:
-            nums[i], nums[zero] = nums[zero], nums[i]
+        if nums[i] != 0:  # num non-zero
+            nums[i], nums[zero] = nums[zero], nums[i]  # swap
             zero += 1
-    """
