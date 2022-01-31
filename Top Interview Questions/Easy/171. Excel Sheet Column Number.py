231c8d6fad072b245474b19ab0a6b9e0c6a93ffb
Samyak Jain <samyak65400@gmail.com>
2022-01-31 18:07:23 +0530

    understanding++

--- Top Interview Questions/Easy/171. Excel Sheet Column Number.py
+++ Top Interview Questions/Easy/171. Excel Sheet Column Number.py
@@ -7,19 +7,21 @@ def titleToNumber(column_title: str) -> int:  # TC = O(n); SC = O(1)
 
     """
     ans = 0
-
-    for i, letter in enumerate(column_title[::-1]):
-
-        ans += (ord(letter)-64) * (26**i)  # (letter_number) * (26**letter_index)
-
+    for letter_index, letter in enumerate(column_title[::-1], start=0):
+        ans += (ord(letter)-64) * (26**letter_index)  # (ord(letter)-64) => letter number
     return ans
     """
 
     # Better ->
 
     ans = 0
-
     for letter in column_title:
-        ans = (26*ans) + (ord(letter)-64)
-
+        ans *= 26
+        ans += ord(letter)-64
     return ans
+
+    # This is similar to how the number 254 could be broken down as this: (2 x 10 x 10) + (5 x 10) + (4).
+    # The reason we use 26 instead of 10 is that 26 is our base.
+
+    # Think of this problem as the same way you'd manually take a binary string and calculate its decimal representation.
+    # Instead of being base 2 it is base 26.
