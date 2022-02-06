8cd3102f795abd4c436348981b9f74d3d2e952ef
Samyak Jain <samyak65400@gmail.com>
2022-02-07 05:27:04 +0530

    understanding++

--- Top Interview Questions/Easy/121. Best Time to Buy and Sell Stock.py
+++ Top Interview Questions/Easy/121. Best Time to Buy and Sell Stock.py
@@ -10,15 +10,17 @@ def maxProfit(prices: List[int]) -> int:
 
     # Kadane's Algo: TC = O(n); SC = O(1)
 
-    min_price = float('inf')  # at first, any price will be less than this
+    cost_price = float('inf')  # at first, any price will be less than this
     max_profit = 0  # at first, any day's profit will be greater than this
 
-    for price in prices:
+    for cur_price in prices:  # n prices
 
-        if price < min_price:
-            min_price = price
+        if cur_price < cost_price:  # buy stock
+            cost_price = cur_price
 
-        elif price-min_price > max_profit:  # price-min_price = current day profit
-            max_profit = price - min_price
+        else:
+            cur_profit = cur_price - cost_price
+            if cur_profit > max_profit:
+                max_profit = cur_profit
 
     return max_profit
