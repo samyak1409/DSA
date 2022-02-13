c079f9bc71060c14298df264c5b691097bbab59a
Samyak Jain <samyak65400@gmail.com>
2022-02-14 02:45:43 +0530

    approaches++; improve.

--- Top Interview Questions/Easy/1. Two Sum.py
+++ Top Interview Questions/Easy/1. Two Sum.py
@@ -8,16 +8,35 @@ from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
 
-    # Using Hash Table: TC = O(n) = SC
+    # 0) Brute-force (TLE): TC = O(n^2); SC = O(1)
 
-    mapping = {}  # num: index
+    """
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i]+nums[j] == target:
+                return [i, j]
+    """
 
-    for index, num in enumerate(nums):
+    # 1) Tried Sorting + Binary Search but didn't work out. (TC = O(n log n); SC = O(n))
 
-        got = mapping.get(target-num)  # target - num = another required num
+    # 2) Hash Map: TC = O(n); SC = O(n)
 
-        if got is None:
-            mapping[num] = index
+    """
+    hashmap = {}  # num: index
+    for i, num in enumerate(nums):
+        hashmap[num] = i
 
-        else:
-            return [got, index]
+    for i, num in enumerate(nums):
+        i_req = hashmap.get(target-num)  # target-num = required num; i_req -> index of required num
+        if i_req is not None and i_req != i:
+            return [i, i_req]
+    """
+
+    # 2.1) Hash Map (Single-pass): TC = O(n); SC = O(n)
+
+    hashmap = {}  # num: index
+    for i, num in enumerate(nums):
+        i_req = hashmap.get(target-num)  # target-num = required num; i_req -> index of required num
+        if i_req is not None:
+            return [i, i_req]
+        hashmap[num] = i
