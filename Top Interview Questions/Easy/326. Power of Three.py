b2cb039db237ca12711238e3b87cfe152769ddd3
Samyak Jain <samyak65400@gmail.com>
2022-02-17 05:40:12 +0530

    revamp

--- Top Interview Questions/Easy/326. Power of Three.py
+++ Top Interview Questions/Easy/326. Power of Three.py
@@ -5,42 +5,58 @@ https://leetcode.com/problems/power-of-three/
 
 def isPowerOfThree(n: int) -> bool:
 
-    # 1) Using Log (Problematic):
+    # 1) Simple Maths (Correct but Problematic in programming): TC = O(log3 (n)); SC = O(1)
 
     """
-    if n < 1:  # non-positive integers
+    if n <= 0:  # non-positive integers
         return False
 
     from math import log
     x = log(n, 3)
+    return x == int(x)
 
-    diff = abs(x-round(x))
-    print(x, round(x), diff)  # debug
-
-    # return diff == 0  # https://leetcode.com/problems/power-of-three/solution/#:~:text=Common%20pitfalls
-
-    from sys import float_info
-    print(float_info.epsilon, diff-float_info.epsilon)  # debug
-
-    return diff-float_info.epsilon <= 0
+    # Can give wrong answer because: https://leetcode.com/problems/power-of-three/solution/#:~:text=Common%20pitfalls
     """
 
-    # 3) Log and Power 👌 (Solves the problem of Method 1): TC = O(?); SC = O(?)
+    # 1.1) Log and Exponent (Power) 👌 (Solves the problem of method 1): TC = O(log3 (n)); SC = O(1)
 
     """
-    if n < 1:
+    if n <= 0:  # non-positive integers
         return False
 
     from math import log
-    x = round(log(n, 3))
-    print(x)  # debug
+    x = round(log(n, 3))  # round the value up
+    return 3**x == n  # reverse check
 
-    return 3**x == n
+    # How does it give correct answer:
+    # case 1) precision error is there: rounding up will correct it, reverse check will return True
+    # case 2) precision errors isn't there:
+    #                                       case 2.1) value from 'log' is a decimal num: 'round' will convert it to int, reverse check will return False
+    #                                       case 2.2) value from 'log' is an int: reverse check will return True
     """
 
     # 2) Divide Loop: TC = O(log3 (n)); SC = O(1)
 
+    """
     while n > 1:
         n /= 3
 
     return n == 1
+    """
+
+    # 3) Thug Life: TC = O(1); SC = O(1)
+
+    # https://leetcode.com/problems/power-of-three/solution/#:~:text=Approach%204%3A%20Integer%20Limitations
+    # https://leetcode.com/problems/power-of-three/discuss/77977/Math-1-liner-no-log-with-explanation
+
+    """
+    from math import log, floor
+    max_n = 2**31 - 1  # Q. constraint: (-2^31 <= n <= 2^31 - 1)
+    max_pow_3 = 3 ** floor(log(max_n, 3))  # = 3 ** floor(19.558822360291316) = 3 ** 19 = 1162261467
+    print(max_pow_3)  # debug
+    return (n > 0) and (max_pow_3 % n == 0)
+    """
+
+    return (n > 0) and (1162261467 % n == 0)
+
+    # Also read: https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2
