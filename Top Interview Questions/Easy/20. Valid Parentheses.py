aa5598bd95d0dfb4f38dca366c819e09e3b6ef1d
Samyak Jain <samyak65400@gmail.com>
2022-02-24 02:59:15 +0530

    cmnts++

--- Top Interview Questions/Easy/20. Valid Parentheses.py
+++ Top Interview Questions/Easy/20. Valid Parentheses.py
@@ -5,20 +5,23 @@ https://leetcode.com/problems/valid-parentheses/
 
 def isValid(s: str) -> bool:
 
-    # Using Stack: TC = O(n) = SC
+    # Using Stack: TC = O(n); SC = O(n)
 
-    if len(s) % 2:  # optimization if odd number of brackets are there
+    if len(s) % 2:  # optimization if odd no. of brackets
         return False
 
-    closed = {'(': ')', '{': '}', '[': ']'}
-    stack = ['']  # adding one element so that stack is empty error don't occur
+    closed = {'(': ')', '{': '}', '[': ']'}  # helper
+    stack = [None]  # added None so that stack doesn't underflow
 
     for bracket in s:
 
-        if closed.get(bracket):  # if type(bracket) = opened
+        # if type of current bracket == opened, push its closed type to the stack (for future equality-check)
+        if bracket in closed.keys():  # closed.keys() = set of opened brackets
             stack.append(closed[bracket])
 
-        elif bracket != stack.pop():
-            return False
+        else:  # (if type of current bracket == closed)
+            # pop an element (top) from stack and compare it with current bracket (type of both = closed), if not equal, means invalid parentheses
+            if bracket != stack.pop():
+                return False
 
-    return stack == ['']
+    return stack == [None]  # check whether the stack is back to its initial value, and return
