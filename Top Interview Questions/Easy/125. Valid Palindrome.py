b92985a453b51f7a979f8b6fb176709dfd0b4161
Samyak Jain <samyak65400@gmail.com>
2022-02-23 23:05:10 +0530

    +approach+improve

--- Top Interview Questions/Easy/125. Valid Palindrome.py
+++ Top Interview Questions/Easy/125. Valid Palindrome.py
@@ -5,20 +5,54 @@ https://leetcode.com/problems/valid-palindrome/
 
 def isPalindrome(s: str) -> bool:
 
-    s = ''.join(filter(lambda char: (65 <= ord(char) <= 90) or (48 <= ord(char) <= 57), s.upper()))
+    # 0) Helper func for all the algos:
 
-    # Method 1 (char by char matching): TC = O(n); SC = O(1)
+    def is_alphanumeric(c: chr) -> bool:
+        return ('a' <= c <= 'z') or ('0' <= c <= '9')  # https://en.wikipedia.org/wiki/ASCII#Printable_characters
+
+    # 1) OTG Char By Char Matching: TC = O(n); SC = O(1)
 
     """
+    l, r = 0, len(s)-1
+
+    while l < r:
+
+        x, y = s[l].lower(), s[r].lower()
+        x_, y_ = is_alphanumeric(x), is_alphanumeric(y)
+
+        if x_ and y_:  # if both chars are alphanumeric
+            if x == y:  # equate
+                l += 1
+                r -= 1
+            else:
+                return False
+
+        # skip char if not alphanumeric:
+        if not x_:
+            l += 1
+        if not y_:
+            r -= 1
+
+    return True
+    """
+
+    # 2) Direct Equating: TC = O(n); SC = O(n)
+
+    """
+    s = ''.join(filter(is_alphanumeric, s.lower()))
+    half = len(s) // 2
+    return s[:half] == s[::-1][:half]
+    # or simply:
+    # return s == s[::-1]  # (O(n/2) = O(n))
+    """
+
+    # 3) Char By Char Matching: TC = O(n); SC = O(1)
+
+    s = ''.join(filter(is_alphanumeric, s.lower()))
+
     for i in range(len(s)//2):
 
         if s[i] != s[-i-1]:
             return False
 
     return True
-    """
-
-    # Method 2 (direct equating): TC = O(n); SC = O(n)
-
-    half = len(s) // 2
-    return s[:half] == s[:-half-1:-1]  # reversed
