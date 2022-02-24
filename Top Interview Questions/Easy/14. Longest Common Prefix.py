8726898717893f34fd95db7e26b2ec63541e7e44
Samyak Jain <samyak65400@gmail.com>
2022-02-24 20:25:05 +0530

    cmnts

--- Top Interview Questions/Easy/14. Longest Common Prefix.py
+++ Top Interview Questions/Easy/14. Longest Common Prefix.py
@@ -8,15 +8,17 @@ from typing import List
 
 def longestCommonPrefix(strs: List[str]) -> str:
 
-    # TC = O(mn); SC = O(n); where n = no. of strings in strs and m = length of the smallest string
+    # Best from https://leetcode.com/problems/longest-common-prefix/solution & https://leetcode.com/problems/longest-common-prefix/discuss:
+
+    # Vertical Scanning (Pythonic): TC = O(mn); SC = O(n); where m = len(LCP) to be (<= len(min(strs, key=len))) and n = len(strs)
 
     lcp = ''
 
-    for tup in zip(*strs):  # * -> unpacking; TC = O(m), this loop will run m times
+    for tup in zip(*strs):  # '*' -> unpacking; this loop will run m times, so TC = O(m)
 
-        # tup will occupy n space every iteration, so SC = O(n)
+        # tup will occupy n space on every iteration, so SC = O(n)
 
-        if len(set(tup)) == 1:  # => all chars same; TC = O(n), set construction
+        if len(set(tup)) == 1:  # => all chars same; set construction, so TC = O(n)
             lcp += tup[0]
 
         else:
