70b7d4ecf206476f2f9c5f0ee6160c29be81a785
Samyak Jain <samyak65400@gmail.com>
2022-07-09 05:30:10 +0530

    5) done 🌚

--- 1) Arrays/5) Sort Colors.py
+++ 1) Arrays/5) Sort Colors.py
@@ -28,3 +28,45 @@ def sortColors(nums: List[int]) -> None:
     nums[:] = [0]*n[0] + [1]*n[1] + [2]*n[2]  # O(n)
     # New Discovery: e.g. 2 is not there in nums, still n[2] will not give an error because n is a Counter object, and it handles that (by default returns 0 value if a key is not there)
     """
+
+    # 2) The "Running But Don't Know How" Algo (one-pass algorithm using constant extra space): TC = O(n); SC = O(1)
+    # https://leetcode.com/problems/sort-colors/discuss/26500/Four-different-solutions/160956
+
+    """
+    n2 = n1 = n0 = -1
+    for num in nums:
+        match num:
+            case 2:
+                n2 += 1
+                nums[n2] = 2
+            case 1:
+                n2 += 1
+                n1 += 1
+                nums[n2] = 2
+                nums[n1] = 1
+            case 0:
+                n2 += 1
+                n1 += 1
+                n0 += 1
+                nums[n2] = 2
+                nums[n1] = 1
+                nums[n0] = 0
+    """
+
+    # 3) Swapping (one-pass algorithm using constant extra space): TC = O(n); SC = O(1)
+    # https://en.wikipedia.org/wiki/Dutch_national_flag_problem
+    # https://leetcode.com/problems/sort-colors/discuss/26472/Share-my-at-most-two-pass-constant-space-10-line-solution/25489; https://leetcode.com/problems/sort-colors/discuss/26481/Python-O(n)-1-pass-in-place-solution-with-explanation
+    # The idea is to sweep all 0s to the left and all 2s to the right, then all 1s are left in the middle.
+
+    i, low, high = 0, 0, len(nums)-1
+    while i <= high:
+        match nums[i]:
+            case 0:
+                nums[i], nums[low] = nums[low], nums[i]
+                low += 1
+                i += 1
+            case 1:
+                i += 1
+            case 2:
+                nums[i], nums[high] = nums[high], nums[i]
+                high -= 1
