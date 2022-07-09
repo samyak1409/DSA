e98636b16c6f317edc22c9f6f4d2d6bf560f76d4
Samyak Jain <samyak65400@gmail.com>
2022-07-10 01:31:18 +0530

    7) brute, optimal (by me)

--- 01) Arrays/007) Rotate Image.py
+++ 01) Arrays/007) Rotate Image.py
@@ -11,4 +11,25 @@ def rotate(matrix: List[List[int]]) -> None:
     Do not return anything, modify matrix in-place instead.
     """
 
-    pass
+    # 0) Brute-force (Using matrix copy): TC = O(n^2); SC = O(n^2)
+
+    """
+    from copy import deepcopy
+    n = len(matrix)
+    matrix_copy = deepcopy(matrix)  # you should know what deepcopy is for 😁
+    for i in range(n):
+        for j in range(n):
+            matrix[j][-i-1] = matrix_copy[i][j]
+    """
+
+    # 1) Transpose & Mirror (Y-axis): TC = O(n^2); SC = O(1)
+    # Intuition: We can observe that, the first row of the output matrix is just the reverse of the first column of input matrix, and so on.
+
+    n = len(matrix)
+    # Transposing:
+    for i in range(n):  # O(n^2)
+        for j in range(i+1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # Mirroring on Y-axis:
+    for i in range(n):  # O(n^2)
+        matrix[i].reverse()
