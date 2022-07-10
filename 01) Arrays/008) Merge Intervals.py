50df6fdbaa70d319a83f54aa3caa52c1b6d8e624
Samyak Jain <samyak65400@gmail.com>
2022-07-10 23:46:18 +0530

    8) done

--- 01) Arrays/008) Merge Intervals.py
+++ 01) Arrays/008) Merge Intervals.py
@@ -8,4 +8,16 @@ from typing import List
 
 def merge(intervals: List[List[int]]) -> List[List[int]]:
 
-    pass
+    # 1) Brute-force = Optimal (Sorting + Linear Merge): TC = O(n*log(n)); SC = O(1)
+
+    intervals.sort(key=lambda x: x[0])  # sort by start value of the intervals; TC = O(n*log(n))
+
+    previous = intervals[0]
+    for i in range(1, len(intervals)):  # TC = O(n)
+        interval = intervals[i]
+        if previous[1] >= interval[0]:  # => intervals are overlapping!
+            previous[1] = max(previous[1], interval[1])  # merging
+        else:
+            yield previous  # adding the non-overlapping intervals to the output
+            previous = interval  # saving the current interval in order to check if it can be merged with the following interval
+    yield previous  # adding the last (overlapping/non-overlapping) interval to the output
