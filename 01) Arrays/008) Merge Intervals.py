de3852c5a513b07d6b7ac1119f3c742add674477
Samyak Jain <samyak65400@gmail.com>
2022-07-11 00:18:45 +0530

    8) added a tried but failed approach

--- 01) Arrays/008) Merge Intervals.py
+++ 01) Arrays/008) Merge Intervals.py
@@ -8,6 +8,32 @@ from typing import List
 
 def merge(intervals: List[List[int]]) -> List[List[int]]:
 
+    # 0) Tried a different logic but Failed (Wrong Answer): TC = O(n^2); SC = O(n)
+    # Only and only due to following case: When intervals do not overlap but are contiguous. e.g.: [[1, 4], [5,6]] :(
+    # Following algorithm can't differentiate between the test cases [[1, 6]] and [[1, 4], [5,6]]
+    # And returns the same output for the both.
+
+    """
+    arr_start, arr_end = float('inf'), float('-inf')
+    for interval in intervals:  # getting smallest and largest number among all intervals; TC = O(n)
+        arr_start, arr_end = min(arr_start, *interval), max(arr_end, *interval)
+
+    arr_len = arr_end - arr_start + 1
+    arr = [False] * arr_len  # initialize an array with all "False"(s); SC = O(n)
+    for interval in intervals:  # TC = O(n^2)
+        start, end = interval[0]-arr_start, interval[1]-arr_start
+        arr[start:end+1] = [True] * (end-start+1)  # insert "True" at all the indices which lie in an interval
+
+    i = 0
+    while i < arr_len:  # TC = O(n)
+        if arr[i]:  # starting of an interval
+            i_ = i  # saving start point
+            while i+1 < arr_len and arr[i+1]:  # while interval continues
+                i += 1
+            yield [i_+arr_start, i+arr_start]  # [start, end] of the merged interval
+        i += 1
+    """
+
     # 1) Brute-force = Optimal (Sorting + Linear Merge): TC = O(n*log(n)); SC = O(1)
 
     intervals.sort(key=lambda x: x[0])  # sort by start value of the intervals; TC = O(n*log(n))
