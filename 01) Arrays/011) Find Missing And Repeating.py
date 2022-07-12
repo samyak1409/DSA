efd6b15f35f6258ba775966d38881876a0a53956
Samyak Jain <samyak65400@gmail.com>
2022-07-13 01:13:46 +0530

    11) init

---
+++ 01) Arrays/011) Find Missing And Repeating.py
@@ -0,0 +1,11 @@
+"""
+https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1
+"""
+
+
+from typing import List
+
+
+def findTwoElement(arr: List[int], n: int) -> int:
+
+    pass
