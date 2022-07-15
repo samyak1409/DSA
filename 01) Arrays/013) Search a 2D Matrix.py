a41ee720cee764f9b4ec0dc0ec85e5ba108dcdc8
Samyak Jain <samyak65400@gmail.com>
2022-07-16 03:43:03 +0530

    13) brute, better, optimal

--- 01) Arrays/013) Search a 2D Matrix.py
+++ 01) Arrays/013) Search a 2D Matrix.py
@@ -5,3 +5,37 @@ https://leetcode.com/problems/search-a-2d-matrix
 
 def searchMatrix(matrix: list[list[int]], target: int) -> bool:
     """"""
+
+    # 0) Brute-force (Linear Search): TC = O(m*n); SC = O(1)
+
+    """
+    for row in matrix:
+        if target in row:
+            return True
+    return False  # if target is not found above
+    """
+
+    # 1) Better (Finding the Target Row + Linear Search): TC = O(m+n); SC = O(1)
+
+    """
+    for row in matrix:
+        if row[-1] >= target:  # if last element of any row >= target => the target must be in this particular row only!
+            return target in row
+    return False  # if target is not found above (only when target > matrix[-1][-1])
+    """
+
+    # 2) Optimal (Binary Search on whole Matrix): TC = O(log(m*n)); SC = O(1)
+    # https://en.wikipedia.org/wiki/Binary_search_algorithm#Procedure
+
+    m, n = len(matrix), len(matrix[0])
+    low, high = 0, m*n - 1
+    while low <= high:
+        mid = (low+high) // 2
+        integer = matrix[mid // n][mid % n]
+        if integer == target:
+            return True
+        elif integer < target:
+            low = mid + 1
+        else:  # (if integer > target)
+            high = mid - 1
+    return False
