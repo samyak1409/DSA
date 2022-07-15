cbbd28f948285a2855757f85e57370c16d8e9f21
Samyak Jain <samyak65400@gmail.com>
2022-07-15 19:49:49 +0530

    12) optimal (merge sort 👌)

--- 01) Arrays/012) Inversion of Array.py
+++ 01) Arrays/012) Inversion of Array.py
@@ -8,9 +8,36 @@ def inversionCount(arr: list[int], n: int) -> int:
 
     # 0) Brute-force (Two Loops) (TLE): TC = O(n^2); SC = O(1)
 
+    """
     count = 0
     for i in range(n):  # go through all the elements in the array one by one
         for j in range(i+1, n):  # for every element, check for all the elements on the right
             if arr[j] < arr[i]:  # "Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j."
                 count += 1
     return count
+    """
+
+    # 1) Using Merge Sort: TC = O(n*log(n)); SC = O(n)
+    # Prerequisite: https://github.com/samyak1409/python-lab-assignments/blob/main/10/a.py (Merge Sort Algorithm)
+
+    def get_count(array: list[int], length: int) -> int:
+        count = 0
+        if length > 1:
+            # Step1) Dividing:
+            mid_index = length // 2
+            left, right = array[:mid_index], array[mid_index:]
+            count = get_count(array=left, length=mid_index) + get_count(array=right, length=length-mid_index)
+            # Step2) Merging:
+            i = j = 0
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    array[i+j] = left[i]
+                    i += 1
+                else:
+                    array[i+j] = right[j]
+                    j += 1
+                    count += (len(left)-i)  # when element from right list (right[j]) is considered before all left[i:] => total len(left)-i inversions will be there (only for this particular right[j])
+            array[i+j:] = left[i:] or right[j:]
+        return count
+
+    return get_count(array=arr.copy(), length=n)  # copy() so that input array remains unmodified
