4f4720e2a910a5c22f5ccd44148a21fc87e27d15
Samyak Jain <samyak65400@gmail.com>
2022-07-23 19:14:00 +0530

    19) brute

--- 01) Arrays/019) Two Sum.py
+++ 01) Arrays/019) Two Sum.py
@@ -5,3 +5,11 @@ https://leetcode.com/problems/two-sum
 
 def twoSum(nums: list[int], target: int) -> list[int]:
     """"""
+
+    # 0) Brute-force (Nested Loop): TC = O(n^2); SC = O(1)
+
+    n = len(nums)
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
