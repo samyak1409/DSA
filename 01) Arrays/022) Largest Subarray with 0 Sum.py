b9cad9b3293e2a0efb7201b4e33e4e53838ac0c9
Samyak Jain <samyak65400@gmail.com>
2022-08-02 23:57:45 +0530

    22) init

---
+++ 01) Arrays/022) Largest Subarray with 0 Sum.py
@@ -0,0 +1,14 @@
+"""
+https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+"""
+
+
+def max_len(n: int, arr: list[int]) -> int:
+    """"""
+
+    # Easy https://youtu.be/xmguZ6GbatA
+
+
+# Similar Questions:
+# https://leetcode.com/problems/subarray-sum-equals-k
+# https://leetcode.com/problems/contiguous-array
