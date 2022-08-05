6419d2222255407b610040d40cd920b6027004c4
Samyak Jain <samyak65400@gmail.com>
2022-08-05 13:38:55 +0530

    24) brute

--- 01) Arrays/024) Longest Substring Without Repeating Characters.py
+++ 01) Arrays/024) Longest Substring Without Repeating Characters.py
@@ -5,3 +5,21 @@ https://leetcode.com/problems/longest-substring-without-repeating-characters
 
 def length_of_longest_substring(s: str) -> int:
     """"""
+
+    # 0) Brute-force (Nested Loop & HashSet): TC = O(n^2); SC = O(1) because Q. constraint
+    #                                                      "s consists of English letters, digits, symbols and spaces."
+
+    n = len(s)
+    longest = 0
+    for i in range(n):
+        occurred = set()  # for O(1) lookup
+        count = 0
+        for j in range(i, n):
+            char = s[j]
+            if char not in occurred:
+                count += 1
+                occurred.add(char)
+            else:  # when a char is repeated
+                break  # start the loop with next starting
+        longest = max(longest, count)
+    return longest
