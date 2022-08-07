a6fc1a17ba9b0b26d95c0fa8b3a7b6553fae06b0
Samyak Jain <samyak65400@gmail.com>
2022-08-07 18:59:03 +0530

    1.2.2) brute

--- 01) Arrays/002) Pascal's Triangle II.py
+++ 01) Arrays/002) Pascal's Triangle II.py
@@ -5,3 +5,18 @@ https://leetcode.com/problems/pascals-triangle-ii
 
 def get_row(row_index: int) -> list[int]:
     """"""
+
+    # Explanation of all the solutions:
+    # https://github.com/samyak1409/DSA/blob/main/01%29%20Arrays/002%29%20Pascal%27s%20Triangle%20.py
+
+    # 0) Brute-force (Nested Loop): TC = O(n^2); SC = O(n)
+    # Surprisingly: https://leetcode.com/submissions/detail/767581085
+
+    row = [1]  # init.
+    for _ in range(row_index):
+        row = [i+j for i, j in zip(row+[0], [0]+row)]
+    return row
+
+    # 1) Better (Combinations): TC = O(n^2); SC = O(1)
+
+    # 2) Optimal (Pattern): TC = O(n); SC = O(1)
