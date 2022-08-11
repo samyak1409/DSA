ed479eafe58010952f67678b4834849e4d36f60d
Samyak Jain <samyak65400@gmail.com>
2022-08-11 05:37:46 +0530

    1.23) improved

--- 01) Arrays/023) Count Subarrays with Given XOR.py
+++ 01) Arrays/023) Count Subarrays with Given XOR.py
@@ -6,7 +6,7 @@ https://www.codingninjas.com/codestudio/problems/1115652
 def sub_arrays_xor(arr: list[int], x: int) -> int:
     """"""
 
-    # This problem is solved using the same outer logic as
+    # This problem is solved using the same core logic as
     # https://github.com/samyak1409/DSA/blob/main/01%29%20Arrays/022%29%20Largest%20Subarray%20with%200%20Sum.py.
 
     # Properties of XOR:
@@ -17,36 +17,35 @@ def sub_arrays_xor(arr: list[int], x: int) -> int:
 
     """
     n = len(arr)
-    count = 0
+    ans = 0
     for i in range(n):  # choose every element one by one
         xor_value = 0  # init
         for j in range(i, n):
             xor_value ^= arr[j]  # increase the length of sub-array
-            if xor_value == x:  # if xor_value of sub-array arr[i:j+1] = x
-                count += 1
-    return count
+            if xor_value == x:  # if xor_value of sub-array arr[i:j+1] == x
+                ans += 1
+    return ans
     """
 
-    # 1) Optimal (Prefix-XOR & HashSet): TC = O(n); SC = O(n)
+    # 1) Optimal (Prefix-XOR & HashMap): TC = O(n); SC = O(n)
     # Explanation: https://youtu.be/lO9R5CaGRPY
 
     prefix_xor = 0
-    count = 0
-    hashset = {prefix_xor: 1}  # O(1) lookup
-    # initializing with "prefix_xor: 1" because:
+    ans = 0
+    frequency = {prefix_xor: 1}  # for O(1) lookup; initializing with "prefix_xor: 1" because:
     # dry run the algo with input (arr=[6, 6], x=6), you'll get the answer.
     for element in arr:  # O(n)
         prefix_xor ^= element
         # using the inverse logic:
-        y = prefix_xor ^ x
-        if y in hashset:  # O(1)
-            count += hashset[y]
-        # add/update prefix_xor occurred:
-        hashset[prefix_xor] = hashset.get(prefix_xor, 0) + 1
-    return count
+        if (freq := frequency.get(prefix_xor ^ x)) is not None:  # O(1)
+            ans += freq  # ✔✔ if a pair is made with a prefix_xor, then it will also satisfy with any/every previous
+            # occurrences of that particular prefix_xor
+        # add/update frequency of prefix_xor:
+        frequency[prefix_xor] = frequency.get(prefix_xor, 0) + 1
+    return ans
 
     # Explanation of "inverse logic":
-    # If at any iteration, (prefix_xor ^ x) = y is present in the hashset,
+    # If at any iteration, (prefix_xor ^ x) = y is present in the hashmap,
     # this means that from index (index(y)+1) till current index i.e. index(element),
     # the sub-array's XOR is x, that's why we got current prefix_xor from y (if a ^ b = c, then a = b ^ c).
     # Solve this problem for complete understanding: https://leetcode.com/problems/subarray-sum-equals-k
