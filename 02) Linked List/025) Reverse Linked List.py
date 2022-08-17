129d07764cf6e6179cc5f1e13c46a5f95833893f
Samyak Jain <samyak65400@gmail.com>
2022-08-18 02:16:59 +0530

    25) init

---
+++ 02) Linked List/025) Reverse Linked List.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/reverse-linked-list
+"""
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, nxt=None):
+        self.val = val
+        self.nxt = nxt
+
+
+def reverse_list(head: Optional[ListNode]) -> Optional[ListNode]:
+    """"""
