dd0d61d94c8b0171596c4463472094e8118b19bb
Samyak Jain <samyak65400@gmail.com>
2022-08-18 19:57:09 +0530

    25) done

--- 02) Linked List/025) Reverse Linked List.py
+++ 02) Linked List/025) Reverse Linked List.py
@@ -15,3 +15,60 @@ class ListNode:
 
 def reverse_list(head: Optional[ListNode]) -> Optional[ListNode]:
     """"""
+
+    # 0) Brute-force (Changing the Values): TC = O(n); SC = O(n)
+
+    """
+    # Saving:
+    head_ = head  # copying head coz singly LL
+    vals = []
+    while head_:
+        vals.append(head_.val)
+        head_ = head_.nxt  # ++
+
+    # Changing:
+    head_ = head
+    for i in range(-1, -len(vals)-1, -1):  # reverse traverse the saved vals
+        head_.val = vals[i]
+        head_ = head_.nxt  # ++
+
+    return head
+    """
+
+    # 1) Optimal (Changing the Direction of Pointers): TC = O(n); SC = O(1)
+    # https://media.geeksforgeeks.org/wp-content/cdn-uploads/RGIF2.gif
+    # https://leetcode.com/problems/reverse-linked-list/discuss/58127/Python-Iterative-and-Recursive-Solution
+
+    # 1.2) It can also be done Recursively: TC = O(n); SC = O(n) {recursion stack}
+    # O(n) space, still writing, why? For Recursion practice!
+    """
+    def reverse(curr, prev=None):
+        # '''Recursively reverses a singly LL and returns the head node.'''
+        if not curr:  # base case: traversed whole LL
+            return prev  # as the direction of pointers are reversed, `prev` (last node) basically is head now!
+        next_curr = curr.nxt  # save ref to the next of curr node for updating in the end
+        curr.nxt = prev  # reverse the pointer direction from next to prev node
+        return reverse(curr=next_curr, prev=curr)  # recurse with updated `curr` & `prev`
+
+    return reverse(curr=head)
+    """
+
+    # 1.1) Iteratively:
+    """
+    prev = None  # at first (as the end of LL is `None`)
+
+    while head:  # iterate till the end
+
+        next_head = head.nxt  # save ref to the next of curr node (`head`) for updating in the end
+        head.nxt = prev  # reverse the pointer direction from next to prev node
+
+        prev = head  # update the prev node
+        head = next_head  # move the head node to the next
+
+    return prev  # as the direction of pointers are reversed, `prev` (last node) basically is head now!
+    """
+    # In short:
+    prev = None  # at first (as the end of LL is `None`)
+    while head:  # iterate till the end
+        head.nxt, head, prev = prev, head.nxt, head  # reverse ptr direction + update `head` + update `prev`
+    return prev  # as the direction of pointers are reversed, `prev` (last node) basically is head now!
