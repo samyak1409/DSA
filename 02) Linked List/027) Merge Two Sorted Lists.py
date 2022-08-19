9dc889c7a409a98a7852531646e83877056c29ed
Samyak Jain <samyak65400@gmail.com>
2022-08-19 09:28:22 +0530

    27) init

---
+++ 02) Linked List/027) Merge Two Sorted Lists.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/merge-two-sorted-lists
+"""
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
+def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    """"""
