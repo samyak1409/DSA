1cfe62eeb7ae7fea4864fe3a21b461f9281b649b
Samyak Jain <samyak65400@gmail.com>
2022-08-23 07:00:00 +0530

    29) init

---
+++ 02) Linked List/029) Add Two Numbers.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/add-two-numbers
+"""
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
+def add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    """"""
