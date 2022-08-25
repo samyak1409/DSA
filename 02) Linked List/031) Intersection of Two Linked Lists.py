a574c9cbdebf4b85484500b9042f04213d6e3af7
Samyak Jain <samyak65400@gmail.com>
2022-08-25 06:38:52 +0530

    31) init

---
+++ 02) Linked List/031) Intersection of Two Linked Lists.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/intersection-of-two-linked-lists
+"""
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def get_intersection_node(headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    """"""
