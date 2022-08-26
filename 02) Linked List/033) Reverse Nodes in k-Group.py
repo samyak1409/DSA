0d1155d3f04d2840626fb8fe111e5c0a91159f47
Samyak Jain <samyak65400@gmail.com>
2022-08-26 21:10:56 +0530

    33) diff., will do in end

---
+++ 02) Linked List/033) Reverse Nodes in k-Group.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/reverse-nodes-in-k-group
+"""
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
+def reverse_k_group(head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    """"""
