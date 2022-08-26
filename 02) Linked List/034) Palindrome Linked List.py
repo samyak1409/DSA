f4c4eaa9286a640e286f8819509a97072ba362b8
Samyak Jain <samyak65400@gmail.com>
2022-08-26 21:40:06 +0530

    34) init

---
+++ 02) Linked List/034) Palindrome Linked List.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/palindrome-linked-list
+"""
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
+def is_palindrome(head: Optional[ListNode]) -> bool:
+    """"""
