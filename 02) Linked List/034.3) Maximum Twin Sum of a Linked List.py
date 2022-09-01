03ca1a144c6439dcb39a59a2f1d90b4a940703f5
Samyak Jain <samyak65400@gmail.com>
2022-09-01 21:35:15 +0530

    34.3) init

---
+++ 02) Linked List/034.3) Maximum Twin Sum of a Linked List.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list
+"""
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
+def pair_sum(head: Optional[ListNode]) -> int:
+    """"""
