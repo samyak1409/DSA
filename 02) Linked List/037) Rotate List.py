6e8053afe946fcf659b65aab919907236c813c52
Samyak Jain <samyak65400@gmail.com>
2022-09-03 08:44:28 +0530

    37) init

---
+++ 02) Linked List/037) Rotate List.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/rotate-list
+"""
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
+def rotate_right(head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    """"""
