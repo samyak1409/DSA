376d8f0e7df733bb6585007ec7092ace4a005375
Samyak Jain <samyak65400@gmail.com>
2022-09-05 02:17:22 +0530

    38) init

---
+++ 02) Linked List/038) Copy List with Random Pointer.py
@@ -0,0 +1,23 @@
+"""
+https://leetcode.com/problems/copy-list-with-random-pointer
+"""
+
+
+from typing import Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int, next_: 'Node' = None, random: 'Node' = None):
+        self.val = val
+        self.next = next_
+        self.random = random
+
+
+def copy_random_list(head: Optional[Node]) -> Optional[Node]:
+    """"""
+
+    pass
+
+
+# Similar Question: https://leetcode.com/problems/clone-graph
