759d88aa8520c63e06b31d3bf75a67df2e6d1796
Samyak Jain <samyak65400@gmail.com>
2022-09-06 06:21:59 +0530

    38) done

--- 02) Linked List/038) Copy List with Random Pointer.py
+++ 02) Linked List/038) Copy List with Random Pointer.py
@@ -22,50 +22,95 @@ def copy_random_list(head: Optional[Node]) -> Optional[Node]:
     # 0.1) Mapping using Indices:
     """
     node = head  # copy for traversing
-    to_head2 = node2 = Node(val=0)  # `to_head2`: for easy pointing to head; `node2`: copy for traversing
-    list1 = []  # list1[i] = random node of i-th node in original LL
+    to_new_head = new_node = Node(val=0)  # `to_new_head`: for easy pointing to head; `new_node`: copy for traversing
+    old_randoms = []  # old_randoms[i] = random node of i-th node in original LL
     mapping = {}  # node: index -> node in original LL & it's index
     index = 0  # of LL node
-    list2 = []  # list2[i] = random node of i-th node in copied LL
+    new_randoms = []  # new_randoms[i] = random node of i-th node in copied LL
     while node:
-        node2.next = Node(val=node.val)  # construct node with val
-        list1.append(node.random)
+        new_node.next = Node(val=node.val)  # construct new node with val
+        old_randoms.append(node.random)
         mapping[node] = index
-        list2.append(node2.next)
-        node, node2, index = node.next, node2.next, index+1  # ++
+        new_randoms.append(new_node.next)
+        node, new_node, index = node.next, new_node.next, index+1  # ++
     mapping[None] = index
-    list2.append(None)
+    new_randoms.append(None)
 
-    node2 = to_head2.next  # copy for traversing
+    new_node = to_new_head.next  # copy for traversing
     index = 0  # of LL node
-    while node2:
-        node2.random = list2[mapping[list1[index]]]  # point random
-        node2, index = node2.next, index+1  # ++
+    while new_node:
+        new_node.random = new_randoms[mapping[old_randoms[index]]]  # point random
+        new_node, index = new_node.next, index+1  # ++
 
-    return to_head2.next
+    return to_new_head.next
     """
     # 0.2) Easier (and Beautiful): Mapping Nodes Directly:
+    # Just iterate the linked list and create copies of the nodes on the go. Since a node can be referenced from
+    # multiple nodes due to the random pointers, ensure you are not making multiple copies of the same node.
+    # You may want to use extra space to keep old_node ---> new_node mapping to prevent creating multiple copies of the
+    # same node.
     # https://leetcode.com/problems/copy-list-with-random-pointer/discuss/43488/Java-O(n)-solution
     # https://leetcode.com/problems/copy-list-with-random-pointer/discuss/811151/Extremely-simple-solution-using-C++
     """
     node = head  # copy for traversing
-    copied = {None: None}  # original_node: copied_node
+    new = {None: None}  # original_node: copied_node
     while node:
-        copied[node] = Node(val=node.val)  # construct node with val and save
+        new[node] = Node(val=node.val)  # construct new node with val and save
         node = node.next  # ++
 
     node = head  # copy for traversing
     while node:
-        copied[node].next = copied[node.next]  # point next
-        copied[node].random = copied[node.random]  # point random
+        new[node].next = new[node.next]  # point next
+        new[node].random = new[node.random]  # point random
         node = node.next  # ++
 
-    return copied[head]
+    return new[head]
     """
 
-    # 1) Optimal (): TC = O(n); SC = O(1)
+    # 1) Optimal (Just Attach New Nodes in Right of Old Nodes): TC = O(n); SC = O(1)
+    # We can avoid using extra space for old_node ---> new_node mapping by tweaking the original linked list. Simply 
+    # interweave the nodes of the old and copied list. For example: Old List: A -> B -> C -> D InterWeaved List: 
+    # A -> A' -> B -> B' -> C -> C' -> D -> D'
+    # The interweaving is done using next pointers, and we can make use of inter-weaved structure to get the correct
+    # reference nodes for random pointers.
+    # https://leetcode.com/problems/copy-list-with-random-pointer/discuss/43491/A-solution-with-constant-space-complexity-O(1)-and-linear-time-complexity-O(N)
+    # https://leetcode.com/problems/copy-list-with-random-pointer/discuss/1059181/C++-or-Three-Pass-or-O(n)-0ms-Beats-100-or-Explanation-(with-example)
 
-    pass
+    # Step 1) Create and Attach:
+    node = head  # copy for traversing
+    while node:
+        new_node = Node(val=node.val)  # construct new node with val
+        node.next, new_node.next = new_node, node.next  # and attach to next of old node
+        node = new_node.next  # +2
+
+    # Step 2) Link Random Pointers (for which we're doing all this):
+    node = head  # copy for traversing
+    while node:
+        node.next.random = node.random.next if node.random else None  # point random; BEAUTIFUL!;
+        # `node.random.next if node.random else None` -> handling when node.random points to None
+        node = node.next.next  # +2
+
+    # Step 3) Detach and Link Next Pointers:
+    '''
+    new_head = head.next  # save ref to return before detachment
+    node = head  # copy for traversing
+    while node:
+        node.next, node.next.next = node.next.next, node.next.next.next if node.next.next else None  # detach and point
+        # next; `node.next.next.next if node.next.next else None` -> handling last node
+        node = node.next  # +2
+
+    return new_head
+    '''
+    # Why above one is not working?
+    # "AttributeError: 'NoneType' object has no attribute 'next'
+    #     node.next, node.next.next = node.next.next, node.next.next.next if node.next.next else None"
+    to_new_head = new_node = Node(val=0)  # `to_new_head`: for easy pointing to head; `new_node`: copy for traversing
+    node = head  # copy for traversing
+    while node:
+        node.next, new_node.next = node.next.next, node.next  # detach and point
+        node, new_node = node.next, new_node.next  # +2
+
+    return to_new_head.next
 
 
 # Similar Question: https://leetcode.com/problems/clone-graph
