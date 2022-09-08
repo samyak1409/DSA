24806a6364af938a2df2ed42479382213632625e
Samyak Jain <samyak65400@gmail.com>
2022-09-08 11:48:21 +0530

    40) hard problem

---
+++ 01) Arrays/040) Trapping Rain Water.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/trapping-rain-water
+"""
+
+
+def trap(height: list[int]) -> int:
+    """"""
+
+
+# Similar Questions:
+# https://leetcode.com/problems/container-with-most-water
+# https://leetcode.com/problems/product-of-array-except-self
