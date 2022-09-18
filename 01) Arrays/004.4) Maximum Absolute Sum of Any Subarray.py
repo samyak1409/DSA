f2241e3111421ce62863246ab95582ab739dac2c
Samyak Jain <samyak65400@gmail.com>
2022-09-19 04:19:12 +0530

    4) re-done

--- 01) Arrays/004.4) Maximum Absolute Sum of Any Subarray.py
+++ 01) Arrays/004.4) Maximum Absolute Sum of Any Subarray.py
@@ -19,39 +19,47 @@ def max_absolute_sum(nums: list[int]) -> int:
     return max_abs
     """
 
-    # 1.1) Optimal (Kadane's Algo): TC = O(n); SC = O(1)
+    # 1) Optimal (Kadane's Algo): TC = O(n); SC = O(1)
     # What if we asked for maximum sum, not absolute sum?
     # It's a standard problem that can be solved by Kadane's algorithm.
     # The key idea is the max absolute sum will be either the max sum or the min sum.
     # So just run kadane twice, once calculating the max sum and once calculating the min sum.
+    # `1.0)` of https://github.com/samyak1409/DSA/blob/main/01%29%20Arrays/004%29%20Maximum%20Subarray.py will work!
+    # Why? Because here we're allowed to choose an empty subarray as answer if max abs sum < 0.
+    # "Return the maximum absolute sum of any (POSSIBLY EMPTY) subarray of nums."
 
     """
-    curr_max = 0
-    curr_min = 0
-    max_sum = float('-inf')
-    min_sum = float('+inf')
+    maxi = curr = 0
     for num in nums:
-        curr_max += num
-        curr_min += num
-        max_sum = max(max_sum, curr_max)
-        min_sum = min(min_sum, curr_min)
-        curr_max = max(curr_max, 0)
-        curr_min = min(curr_min, 0)
-    return max(max_sum, -min_sum)
+        curr = max(curr+num, 0)
+        maxi = max(maxi, curr)
+    mini = curr = 0
+    for num in nums:
+        curr = min(curr+num, 0)
+        mini = min(mini, curr)
+    return max(maxi, -mini)
     """
-    # Or just:
+    # Combined:
+    """
+    maxi = mini = curr_maxi = curr_mini = 0
+    for num in nums:
+        curr_maxi, curr_mini = max(curr_maxi+num, 0), min(curr_mini+num, 0)
+        maxi, mini = max(maxi, curr_maxi), min(mini, curr_mini)
+    return max(maxi, -mini)
+    """
+    # Minor Improvement:
     # https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/discuss/1052471/C++Java:-Find-Max-and-Min-Sum
-    max_sum = min_sum = max_abs_sum = 0
+    ans = curr_maxi = curr_mini = 0
     for num in nums:
-        max_sum = max(max_sum+num, 0)  # new_max_sum = max(old_max_sum+curr_num, 0); 0 means reset
-        min_sum = min(min_sum+num, 0)  # new_min_sum = min(old_min_sum+curr_num, 0); 0 means reset
-        max_abs_sum = max(max_abs_sum, max(max_sum, -min_sum))
-    return max_abs_sum
-    # We aren't tracking current sum, still why working? Because abs sum can't be < 0.
+        curr_maxi, curr_mini = max(curr_maxi+num, 0), min(curr_mini+num, 0)
+        ans = max(ans, max(curr_maxi, -curr_mini))
+    return ans
 
-    # 1.2) Optimal (Prefix Sum): TC = O(n); SC = O(1)
+    # Another Solution:
+    # Optimal (Prefix Sum): TC = O(n); SC = O(1)
     # Just find the maximum prefix sum and the minimum prefix sum. Return max - min.
     # https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/discuss/1052527/JavaC++Python-O(1)-Space
     # The proof idea behind the lee's intuition is using properties of modulus |a-b| = max(a,b) - min(a,b).
     # The sum of subarray arr[i:j]: S(i,j) = S(0,j) - S(0,i) and we want to maximize : |S(i,j)| = |S(0,j) - S(0,i)| =
     # max(S(0, i), S(0, j)) - min(S(0, i), S(0, j)) => max prefix sum - min prefix sum.
+    # -https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/discuss/1052527/JavaC++Python-O(1)-Space/1060161
