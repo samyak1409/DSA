820a6f72b0dd9803e9a81a7f5e4c1ae09f1baeb0
Samyak Jain <samyak65400@gmail.com>
2022-09-20 05:56:26 +0530

    7) improved doc

--- 01) Arrays/007) Rotate Image.py
+++ 01) Arrays/007) Rotate Image.py
@@ -8,8 +8,8 @@ def rotate(matrix: list[list[int]]) -> None:
     Do not return anything, modify matrix in-place instead.
     """
 
-    # You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
-    # DO NOT allocate another 2D matrix and do the rotation.
+    # "You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+    # DO NOT allocate another 2D matrix and do the rotation."
     # -1) Not Allowed (Using Matrix Copy): TC = O(n^2); SC = O(n^2)
 
     """
@@ -22,9 +22,20 @@ def rotate(matrix: list[list[int]]) -> None:
             matrix[j][-i-1] = matrix_copy[i][j]
     """
 
-    # 1) Optimal (Transpose & Mirror (Y-axis)): TC = O(n^2); SC = O(1)
+    # 1) Optimal: TC = O(n^2); SC = O(1)
+
+    # 1.1) Rotate Groups of Four Cells:
+    # Intuition: Observe how the cells move in groups when we rotate the image.
+    # https://leetcode.com/problems/rotate-image/solution/#approach-1-rotate-groups-of-four-cells
+
+    # 1.2) Transpose & Mirror (Y-axis):
+    # The most elegant solution for rotating the matrix is to firstly reverse the matrix around the main
+    # diagonal, and then reverse it from left to right. These operations are called transpose and reflect in linear
+    # algebra.
     # Intuition: We can observe that, the first row of the output matrix is just the reverse of the first column of
     # input matrix, and so on.
+    # https://leetcode.com/problems/rotate-image/solution/#approach-2-reverse-on-diagonal-and-then-reverse-left-to-right
+    # https://leetcode.com/problems/rotate-image/discuss/18872/A-common-method-to-rotate-the-image
 
     n = len(matrix)
     # Transposing:
@@ -34,6 +45,22 @@ def rotate(matrix: list[list[int]]) -> None:
     # Mirroring on Y-axis:
     for i in range(n):  # O(n^2)
         matrix[i].reverse()
+
+    # Even though this approach does twice as many reads and writes as approach 1, most people would consider it a
+    # better approach because the code is simpler, and it is built with standard matrix operations that can be found in
+    # any matrix library.
+
+    # Interesting Implementations of this Approach:
+    # https://leetcode.com/problems/rotate-image/discuss/18884/Seven-Short-Solutions-(1-to-7-lines)
+
+    # Bonus Question: What would happen if you reflect and then transpose? Would you still get the correct answer?
+    # Nope, it'll rotate -90° Clockwise = 90° Anti-Clockwise.
+
+    # Rotations for all the combinations:
+    # Transpose & Mirror (Y-axis): 90° Clockwise
+    # Mirror (Y-axis) & Transpose: 90° Anti-Clockwise
+    # Transpose & Mirror (X-axis): 90° Anti-Clockwise
+    # Mirror (X-axis) & Transpose: 90° Clockwise
 
 
 # Similar Question: https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation
--- 01) Arrays/007.1) Determine Whether Matrix Can Be Obtained By Rotation.py
+++ 01) Arrays/007.1) Determine Whether Matrix Can Be Obtained By Rotation.py
@@ -6,24 +6,32 @@ https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotati
 def find_rotation(mat: list[list[int]], target: list[list[int]]) -> bool:
     """"""
 
-    # 1) Brute-force = Optimal (Rotate & Check 3 Times): TC = O(n^2); SC = O(1)
+    # What is the maximum number of rotations you have to check?
+    # Is there a formula you can use to rotate a matrix 90 degrees?
 
-    if mat == target:  # 0° / 360°
-        return True
+    # 1) Brute-force = Optimal (Check & Rotate 4 Times): TC = O(n^2); SC = O(1)
 
-    n = len(mat)
-    for _ in range(3):  # 90°, 180°, 270°
-        # Rotate 90° (clockwise):
-        # https://github.com/samyak1409/DSA/blob/7cbe5e00f474eb6a0aee5e0b58d66296a59604c3/01%29%20Arrays/007%29%20Rotate%20Image.py#L23:
+    # Helper Function:
+    def rotate() -> None:
+        """https://github.com/samyak1409/DSA/blob/main/01%29%20Arrays/007%29%20Rotate%20Image.py"""
         # Transposing:
-        for i in range(n):  # O(n^2)
-            for j in range(i + 1, n):
+        for i in range(n):
+            for j in range(i+1, n):
                 mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
         # Mirroring on Y-axis:
-        for i in range(n):  # O(n^2)
+        for i in range(n):
             mat[i].reverse()
 
+    n = len(mat)
+    for _ in range(4):
+        # Check: 0°, 90°, 180°, 270°
         if mat == target:
             return True
+        # Rotate 90° (clockwise): 90°, 180°, 270°, 360°
+        rotate()  # O(n^2)
 
-    return False
+    # Extra Note: If mat != target after any ([0, 3] => 0°, 90°, 180°, 270°) number of rotations, mat will come be back
+    # to its original orientation.
+
+    # Also see:
+    # https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/discuss/1254089/C++-straightforward-solution-comparing-in-place
