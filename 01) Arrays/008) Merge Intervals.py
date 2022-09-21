bb1d77862ce5b505c80c30f6cbd197ef18d71622
Samyak Jain <samyak65400@gmail.com>
2022-09-21 11:07:00 +0530

    8) add about py

--- 01) Arrays/008) Merge Intervals.py
+++ 01) Arrays/008) Merge Intervals.py
@@ -9,7 +9,7 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
     # 0) [WA] Brute-force (Spilling the nums of all the intervals): TC = O(n^2); SC = O(n)
     # Tried a different logic but failed only and only due to the following case:
     # When intervals do not overlap but are contiguous. e.g.: [[1, 4], [5, 6]]
-    # Following algorithm can't differentiate between the test cases [[1, 6]] and [[1, 4], [5, 6]]
+    # Following algorithm can't differentiate between the test cases [[1, 5], [4, 6]] and [[1, 4], [5, 6]]
     # And returns the same output for the both.
 
     """
@@ -35,8 +35,10 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
 
     # 1) Brute-force = Optimal (Sorting + Linear Merge): TC = O(n*log(n)); SC = O(n) {In Python, sorting is implemented
     # using the Timsort algorithm, which has a worst-case space complexity of O(n)}
+    # https://leetcode.com/problems/merge-intervals/solution/#approach-2-sorting
 
-    intervals = sorted(intervals, key=lambda x: x[0])  # sort by start value of the intervals; new local var created
+    intervals = sorted(intervals, key=lambda interval: interval[0])  # sort by start value of the intervals;
+    # new local var created
 
     prev = intervals[0]
     for i in range(1, len(intervals)):
@@ -47,6 +49,27 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
             yield prev  # adding the non-overlapping intervals to the output
             prev = curr  # updating previous in order to check if it can be merged with the following interval
     yield prev  # adding the last (overlapping/non-overlapping) interval to the output
+
+    # [Extra]:
+    # Not exactly related to this problem, just a new discovery about "Python":
+    # From https://leetcode.com/problems/merge-intervals/discuss/21227/7-lines-easy-Python/21250, found this:
+    # For sequences like List, `a += b` is not same as `a = a + b`.
+    # `_ += _` will actually try to extend the current sequence with the sequence in RHS, whereas `_ = _ + _` will just
+    # behave as expected.
+    """
+    a, b = [1, 2], [3, 4]
+    print(id(a))  # 2002675308992
+    a += b
+    print(a)  # [1, 2, 3, 4]
+    print(id(a))  # 2002675308992
+    a = a + b
+    print(a)  # [1, 2, 3, 4]
+    print(id(a))  # 2002674932608
+    """
+    # Crazy Stuff 🤯
+    # https://stackoverflow.com/questions/2347265/why-does-behave-unexpectedly-on-lists
+    # https://stackoverflow.com/questions/6951792/a-b-not-the-same-as-a-a-b
+    # https://stackoverflow.com/questions/57025897/concatenation-using-the-and-operators-in-python
 
 
 # Similar Questions:
