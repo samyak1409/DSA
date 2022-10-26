69a32e9f62ff12420226ec12e0add365888b4d9c
Samyak Jain <samyak65400@gmail.com>
2022-10-26 18:29:07 +0530

    37) revamp

--- 02) Linked List/037) Rotate List.py
+++ 02) Linked List/037) Rotate List.py
@@ -20,81 +20,66 @@ def rotate_right(head: Optional[ListNode], k: int) -> Optional[ListNode]:
 
     # 1) Optimal (Directly Go to Required Nodes (2) and Change Pointers): TC = O(n); SC = O(1)
 
-    # 1.1) Calculate Len, Fix k, Go to (k+1)-th Node & Change Pointer, and from there, Go to Last Node & Change Pointer:
-    """
-    if not head or not k or not head.next:  # edge cases
-        return head
+    # 1.1) Calc Len, Make Cycle, Fix k (if required), Go to (n-k)-th Node and Unlink the next Ptr:
+    # TC = O(n + n-k`) {k` = k % n}
+    # https://youtu.be/9VPm6nEbVPA?t=242
+    # https://leetcode.com/problems/rotate-list/discuss/348197/96-faster-Simple-python-solution-with-explanation
 
-    # Find the len of LL:
-    node = head
-    n = 0
-    while node:
+    """
+    if not head:  # edge case
+        return
+
+    # Calc Len:
+    node = head  # copy for traversing
+    n = 1
+    while node.next:
         n += 1
         node = node.next  # ++
 
-    k %= n  # remove duplicate rotations
-    if not k:
-        return head
+    # Now the `node` will be the last node in our LL, linking it to the first node (this will make a cycle):
+    node.next = head
 
-    # Go to the required nodes and change the pointers:
-    new_head = head
-    for _ in range(n-k-1):
-        new_head = new_head.next  # ++
-    new_head.next, new_head = None, new_head.next
-    last_node = new_head
-    while last_node.next:
-        last_node = last_node.next  # ++
-    last_node.next = head
+    # Fix k (if required) (basically remove duplicate rotations):
+    k %= n
 
-    return new_head
-    """
-    # 1.2) Calculate Len and Save Ref to Last Node, Fix k, Change Old Last Node Pointer, Go to (k+1)-th Node & Change
-    #      New Last Node Pointer:
-    # https://youtu.be/9VPm6nEbVPA?t=242
-    # https://leetcode.com/problems/rotate-list/discuss/348197/96-faster-Simple-python-solution-with-explanation
-    """
-    if not head or not k or not head.next:  # edge cases
-        return head
-
-    # Find the len of LL and save ref to last node:
-    last_node = head
-    n = 1
-    while last_node.next:
-        n += 1
-        last_node = last_node.next  # ++
-
-    k %= n  # remove duplicate rotations
-    if not k:
-        return head
-
-    # Change old last pointer, go to (n-k)-th node, and change new last pointer:
-    last_node.next = head
-    for _ in range(n-k-1):
-        head = head.next  # ++
-    head.next, head = None, head.next
+    # Go to (n-k)-th node and unlink the next ptr (in order to get the Rotated List):
+    for _ in range(n-k):
+        node = node.next  # ++
+    node.next, head = None, node.next  # unlink, new_head
 
     return head
     """
-    # 1.3) Assume k < n and Continue Solving for this Case, Meanwhile Calculating the Length, and if We Reach End, Call
-    #      the Function Again with Fixed k:
-    if not head or not k or not head.next:  # edge cases
+
+    # 1.2) Assume k < n and Continue Solving for this Case, Meanwhile Calc the Length, and if Reach End, Call Function
+    # again with Fixed k:
+    # TC = O(n) if (k < n) else O(n+n)
+    # -> This approach uses Two Pointers.
+
+    if not head or not k:  # edge cases
         return head
 
-    n = 0  # for k >= n cases
+    n = 0
+
     # Assuming k < n:
     try:
         # Go to (k+1)-th node and meanwhile track len:
-        last_node = head
+        old_last = head  # copy for traversing
         for _ in range(k):
-            last_node = last_node.next  # ++
+            # (if k >= n, AttributeError will be raised here:)
+            old_last = old_last.next  # ++
             n += 1
-        # Go to (n-k)-th & last node using two pointers:
-        new_head = head
-        while last_node.next:
-            new_head, last_node = new_head.next, last_node.next  # ++
+
+        # Go to (n-k)-th & n-th node using two pointers:
+        new_last = head
+        while old_last.next:
+            new_last, old_last = new_last.next, old_last.next  # ++
+
         # Change pointers:
-        new_head.next, new_head, last_node.next = None, new_head.next, head
-        return new_head
-    except AttributeError:
-        # k >= n, calling again with fixed k:
+        old_last.next, new_last.next, head = head, None, new_last.next  # link, unlink, new_head
+
+        return head
+
+    except AttributeError:  # => k >= n
+        # Call again with fixed k:
         return rotate_right(head, k % n)
+        # return self.rotateRight(head, k % n)
