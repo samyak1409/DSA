4fabc84483d62428ae2ad6ae2dfad270f153361f
Samyak Jain <samyak65400@gmail.com>
2022-12-12 01:42:48 +0530

    fix: remove insort

--- 01) Arrays/008.5) Divide Intervals Into Minimum Number of Groups.py
+++ 01) Arrays/008.5) Divide Intervals Into Minimum Number of Groups.py
@@ -33,46 +33,18 @@ def min_groups(intervals: list[list[int]]) -> int:
     # What we can do is:
     # 1) Will only keep interval[1] i.e. end values of intervals in `groups`.
     # 2) And, will keep `groups` sorted, so that we won't need to linear search whole `groups`, but checking the first
-    #    value will only be required.
-
-    # 1.0) Using normal array: O(n^2)
-    """
-    from bisect import insort
-
-    # Divide the intervals into groups sequentially:
-    groups = []
-    for start, end in sorted(intervals):  # go through each interval one by one; O(n^2)
-        '''
-        if groups and groups[0] < start:  # => compatible group is there
-            # updating the existing group with the new interval:
-            groups.pop(0)  # removing
-            insort(a=groups, x=end)  # adding, keeping it sorted
-        else:  # => no compatible group is there
-            insort(a=groups, x=end)  # so add a new group, keeping it sorted
-        '''
-        # Combined:
-        if groups and groups[0] < start:
-            groups.pop(0)  # O(n)
-        insort(a=groups, x=end)  # O(log(n))
-    return len(groups)
-    """
-
-    # 1.1) Using `deque` (https://docs.python.org/3.11/library/collections.html#collections.deque): O(n*log(n))
-    """
-    from collections import deque
-    from bisect import insort
-
-    # Divide the intervals into groups sequentially:
-    groups = deque()
-    for start, end in sorted(intervals):  # go through each interval one by one; O(n*log(n))
-        if groups and groups[0] < start:
-            groups.popleft()  # O(1)
-        insort(a=groups, x=end)  # O(log(n))
-    return len(groups)
-    """
-
-    # 1.2) Using Heap: O(n*log(n))
+    #    value will only be required. Why? Because the first value will be the least end of them all, and
+    #    -> if it's < than current interval start this means we can add the current interval to this group, and we'll
+    #       update the end, and
+    #    -> if it's not, that mean no other following `end` will be < than current interval start (because obviously
+    #       groups is sorted), in this case we'll just create a new group.
+    #    For keeping `groups` sorted, we can use `bisect.insort`, but, "The insort() functions are O(n) because the
+    #    logarithmic search step is dominated by the linear time insertion step."
+    #    -https://docs.python.org/3.11/library/bisect.html#performance-notes, so this will lead us to O(n^2) again,
+    #    Then what can we do? Heaps (Priority Queues)! O(log(n)) Push & Pop.
+    #    (https://stackoverflow.com/questions/38806202/whats-the-time-complexity-of-functions-in-heapq-library#:~:text=heapq%20is%20a%20binary%20heap%2C%20with%20O(log%20n)%20push%20and%20O(log%20n)%20pop.)
     # https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/solutions/2560020/min-heap
+
     from heapq import heappop, heappush
 
     # Divide the intervals into groups sequentially:
@@ -82,5 +54,3 @@ def min_groups(intervals: list[list[int]]) -> int:
             heappop(groups)  # O(log(n))
         heappush(groups, end)  # O(log(n))
     return len(groups)
-
-    # How is this (very) faster than `deque` one??
