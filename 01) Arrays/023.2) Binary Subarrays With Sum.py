ea677dcb160c44abfd4a681898ca67c99c6f1e6a
Samyak Jain <samyak65400@gmail.com>
2022-12-29 02:48:56 +0530

    23.2) optimal

--- 01) Arrays/023.2) Binary Subarrays With Sum.py
+++ 01) Arrays/023.2) Binary Subarrays With Sum.py
@@ -11,6 +11,7 @@ def num_subarrays_with_sum(nums: list[int], goal: int) -> int:
     # 1) Time-Optimal (Prefix Sum & HashMap): TC = O(n); SC = O(n)
     # https://github.com/samyak1409/DSA/blob/main/01%29%20Arrays/023.1%29%20Subarray%20Sum%20Equals%20K.py
 
+    """
     from collections import Counter
     frequency = Counter()  # Counter for easy working with counts
     prefix_sum = 0
@@ -23,7 +24,47 @@ def num_subarrays_with_sum(nums: list[int], goal: int) -> int:
         # any & every previous occurrences of that particular prefix_sum
         frequency[prefix_sum] += 1  # add/update frequency of prefix_sum
     return count
+    """
 
-    # 2) Optimal (Sliding Window using Three Pointers): TC = O(n); SC = O(1)
-    # https://leetcode.com/problems/binary-subarrays-with-sum/solution/#approach-3-three-pointer
+    # 2) Optimal (Sliding Window): TC = O(n); SC = O(1)
     # https://leetcode.com/problems/binary-subarrays-with-sum/discuss/186683/C++JavaPython-Sliding-Window-O(1)-Space
+    # Explanation:
+    # "This is my understanding of the sliding window solution. If there is anything wrong, please point it out.
+    # atMost(A, S) counts the number of the subarrays of `A` the sum of which is at most (less than or equal to) `S`.
+    # Therefore, we can use atMost(A, S) - atMost(A, S-1) to get the number of the subarrays the sum of which is
+    # exactly `S`.
+    # In the atMost function, the `i` to `j` window represents the subarrays. We use the `j` pointer to expand the
+    # window, when the sum of all numbers in the window is bigger than `S`, it's time for us to move the `i` pointer to
+    # shorten the window. Through this process, we can count the number of the subarrays."
+    # -https://leetcode.com/problems/binary-subarrays-with-sum/solutions/186683/c-java-python-sliding-window-o-1-space/comments/950969
+
+    # Helper Function:
+    def at_most(target: int) -> int:
+        """
+        Sliding Window to find the total number of subarrays with at most `target` sum, in the array consisting only
+        of positive integers.
+        """
+        if target < 0:  # we won't find any subarray with -ve `target` because our array only contains +ves
+            return 0
+        count, i, curr = 0, -1, 0
+        for j in range(len(nums)):
+            curr += nums[j]  # extend window
+            while curr > target:  # while sum is > than target, shrink window
+                curr -= nums[i := i+1]
+            count += j-i  # imp: this looks like we're just adding the len of the subarray to the count, yes we're,
+            # because we are counting the subarrays with at most `target` sum, so, total `j-i` subarrays will satisfy
+            # this condition.
+            # https://leetcode.com/problems/binary-subarrays-with-sum/solutions/186683/c-java-python-sliding-window-o-1-space/comments/1220229
+            # you might also ask if this is the case, then shouldn't it be `n*(n+1) // 2` where n = j-i, instead of just
+            # `j-i`, because the total number of subarrays in an array is `n*(n+1) // 2`, to understand this, revise
+            # how for input ([0, 0, 0, 0, 0], 0), the output is 15.
+            # Basically, other subarrays will be covered in other iterations, so in a single iteration, we only count
+            # "len of the subarray" and not "the total number of subarrays in the subarray".
+        return count
+
+    return at_most(goal) - at_most(goal-1)
+
+    # Note that directly finding the answer without using the `at_most` technique and by using
+    # Three Pointer Sliding Window
+    # (https://leetcode.com/problems/binary-subarrays-with-sum/solutions/186800/binary-subarrays-with-sum/#approach-3-three-pointer)
+    # looks complicated, so didn't do it that way.
