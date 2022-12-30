880e328875266cfab2de69c9e730f83eb2b29cd5
Samyak Jain <samyak65400@gmail.com>
2022-12-31 00:41:29 +0530

    cleanup

--- Contests/Weekly Contest 322/3) Minimum Score of a Path Between Two Cities.py
+++ Contests/Weekly Contest 322/3) Minimum Score of a Path Between Two Cities.py
@@ -6,33 +6,24 @@ https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities
 def min_score(n: int, roads: list[list[int]]) -> int:
     """"""
 
-    # 1) Optimal (Traverse Connected Component of `1`): TC = O(n); SC = O(n)
-    # `The score of a path between two cities is defined as the minimum distance of a road in this path.
-    # Return the minimum possible score of a path between cities 1 and n.`
+    # 1) Optimal (Traverse (Search) (Connected Component of) `1`): TC = O(n); SC = O(n)
     # Basically, we just need to return the minimum edge weight b/w the path from 1 to n.
-    # But, wait, see this https://assets.leetcode.com/uploads/2022/10/12/graph22.png, the output will be 2,
-    # because we can move around before going to n in order to minimize score!
+    # But, see this https://assets.leetcode.com/uploads/2022/10/12/graph22.png, the output will be 2,
+    # i.e. we can move around before going to n in order to minimize score!
     # So, that means, we just need to traverse all the vertices from `1` and return the minimum edge weight encountered!
-    # Also, `The cities graph is not necessarily connected.`
-    # But, `The test cases are generated such that there is at least one path between 1 and n.`
+    # Also, "The cities graph is not necessarily connected."
+    # But, "The test cases are generated such that there is at least one path between 1 and n."
     # So, un-connected graph won't even matter! Only Connected Component of `1` will.
-    # (https://en.wikipedia.org/wiki/Connected_component_(graph_theory))
-    # For traversal, https://en.wikipedia.org/wiki/Graph_traversal#Graph_traversal_algorithms.
     # https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/solutions/2875034/python-c-connected-component-of-1-explained
 
-    from collections import defaultdict, deque
+    # "It is straightforward to compute the components of a finite graph in linear time (in terms of the numbers of the
+    # vertices and edges of the graph) using either breadth-first search or depth-first search. In either case, a search
+    # that begins at some particular vertex `v` will find the entire component containing `v` (and no more) before
+    # returning. All components of a graph can be found by looping through its vertices, starting a new breadth-first or
+    # depth-first search whenever the loop reaches a vertex that has not already been included in a previously found
+    # component." -https://en.wikipedia.org/wiki/Connected_component_(graph_theory)#Algorithms
 
-    graph = defaultdict(dict)
-    for u, v, w in roads:
-        graph[u][v] = graph[v][u] = w
-
-    dq = deque([1])  # for O(1) removal from left
-    visited = set()  # for O(1) lookup
-    min_weight = float('inf')  # init
-    while dq:
-        for node, weight in graph[dq.popleft()].items():
-            if node not in visited:
-                dq.append(node), visited.add(node)
-            else:
-                min_weight = min(min_weight, weight)
-    return min_weight
+    # "Note. — If each vertex in a graph is to be traversed by a tree-based algorithm (such as DFS or BFS), then the
+    # algorithm must be called at least once for each connected component of the graph. This is easily accomplished by
+    # iterating through all the vertices of the graph, performing the algorithm on each vertex that is still unvisited
+    # when examined." -https://en.wikipedia.org/wiki/Graph_traversal#Graph_traversal_algorithms
--- Contests/Weekly Contest 323/3) Design Memory Allocator.py
+++ Contests/Weekly Contest 323/3) Design Memory Allocator.py
@@ -6,7 +6,7 @@ https://leetcode.com/problems/design-memory-allocator
 class Allocator:
     """"""
 
-    # 1) Brute-force = Sub-Optimal (Do as told. Linearly allocate and de-allocate):
+    # 1) Brute-force = Optimal (Do as told. Linearly allocate and de-allocate):
     # Can you simulate the process?
     # Use brute force to find the leftmost free block and free each occupied memory unit.
 
@@ -16,9 +16,9 @@ class Allocator:
 
     # "`int allocate(int size, int m_id)` Find the leftmost block of `size` consecutive free memory units and allocate
     # it with the id `m_id`. Return the block's first index. If such a block does not exist, return `-1`."
-    def allocate(self, size: int, m_id: int) -> int:  # TC = O(n^2); SC = O(1)
+    def allocate(self, size: int, m_id: int) -> int:  # TC = O(n); SC = O(1)
         consecutive_units = 0
-        for i, unit in enumerate(self.arr):
+        for i, unit in enumerate(self.arr):  # O(n+n) only because inner loop only executes single time
             if not unit:  # i.e. free
                 consecutive_units += 1
                 if consecutive_units == size:  # found
@@ -31,30 +31,12 @@ class Allocator:
 
     # "`int free(int m_id)` Free all memory units with the id `m_id`. Return the number of memory units you have freed."
     def free(self, m_id: int) -> int:  # TC = O(n); SC = O(1)
-        units_freed = 0
+        units_freed = 0  # count
         for i, unit in enumerate(self.arr):
             if unit == m_id:
                 self.arr[i] = 0  # free
                 units_freed += 1
         return units_freed
-
-    # 2) Optimal (?):
-    # https://leetcode.com/problems/design-memory-allocator/solutions/2899371/c-hashmap-vector-easy-approach
-
-    """
-    # "`Allocator(int n)` Initializes an `Allocator` object with a memory array of size `n`."
-    def __init__(self, n: int):  # TC = O(?); SC = O(?)
-        pass
-
-    # "`int allocate(int size, int m_id)` Find the leftmost block of `size` consecutive free memory units and allocate
-    # it with the id `m_id`. Return the block's first index. If such a block does not exist, return `-1`."
-    def allocate(self, size: int, m_id: int) -> int:  # TC = O(?); SC = O(?)
-        pass
-
-    # "`int free(int m_id)` Free all memory units with the id `m_id`. Return the number of memory units you have freed."
-    def free(self, m_id: int) -> int:  # TC = O(?); SC = O(?)
-        pass
-    """
 
 
 # Your Allocator object will be instantiated and called as such:
