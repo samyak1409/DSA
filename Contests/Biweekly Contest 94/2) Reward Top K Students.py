891fb87fe1ca95474abbf38d12451cb72a8171db
Samyak Jain <samyak65400@gmail.com>
2023-01-06 21:46:34 +0530

    contest 11: medium (crying)

--- Contests/Biweekly Contest 94/2) Reward Top K Students.py
+++ Contests/Biweekly Contest 94/2) Reward Top K Students.py
@@ -3,43 +3,108 @@ https://leetcode.com/problems/reward-top-k-students
 """
 
 
-def top_students(positive_feedback: list[str], negative_feedback: list[str], report: list[str], student_id: list[int], k: int) -> list[int]:
+def top_students(positive_feedback: list[str], negative_feedback: list[str], report: list[str], student_id: list[int],
+                 k: int) -> list[int]:
     """"""
 
-    from heapq import heappush, heappop
-    from itertools import chain
+    # 1) Optimal (Smartly use tuple to achieve required sort order): TC = O(); SC = O()
+    # Hash the positive and negative feedback words separately.
+    # Calculate the points for each student’s feedback.
+    # Sort the students accordingly to find the top k among them.
+    # https://leetcode.com/problems/reward-top-k-students/solutions/2946490/python-use-set-for-fast-access
+    # https://leetcode.com/problems/reward-top-k-students/solutions/2947200/tedious
+
+    # 1.-1) WA because: "In case more than one student has the same points, the one with the lower ID ranks higher."
+    # `dict` preserves the insertion order even then it's WA because `student_id` isn't sorted at the first place.
+    """
+    positive_feedback, negative_feedback = set(positive_feedback), set(negative_feedback)  # allow fast lookup
+
+    points = {}
+    for report_str, s_id in zip(report, student_id):
+        '''
+        point = 0
+        for word in report_str.split():
+            if word in positive_feedback:
+                point += 3
+            elif word in negative_feedback:
+                point -= 1
+        points[s_id] = point
+        '''
+        # One-liner:
+        points[s_id] = sum(3*(word in positive_feedback)-1*(word in negative_feedback) for word in report_str.split())
+    # print(points)  #debugging
+
+    # print(sorted(points, key=lambda id_: points[id_], reverse=True))  #debugging
+    return sorted(points, key=lambda id_: points[id_], reverse=True)[:k]
+    """
+
+    # 1.0) To fix this, we can use sorted dict instead of normal dict:
+    """
+    from sortedcontainers import SortedDict
+
+    positive_feedback, negative_feedback = set(positive_feedback), set(negative_feedback)  # allow fast lookup
+
+    points = SortedDict()
+    for report_str, s_id in zip(report, student_id):
+        '''
+        point = 0
+        for word in report_str.split():
+            if word in positive_feedback:
+                point += 3
+            elif word in negative_feedback:
+                point -= 1
+        points[s_id] = point
+        '''
+        # One-liner:
+        points[s_id] = sum(3*(word in positive_feedback)-1*(word in negative_feedback) for word in report_str.split())
+    # print(points)  #debugging
+
+    # print(sorted(points, key=lambda id_: points[id_], reverse=True))  #debugging
+    return sorted(points, key=lambda id_: points[id_], reverse=True)[:k]
+    """
+
+    # 1.1) Better: Smartly use tuple to achieve required sort order:
+    """
+    positive_feedback, negative_feedback = set(positive_feedback), set(negative_feedback)  # allow fast lookup
+
+    points = {}
+    for report_str, s_id in zip(report, student_id):
+        '''
+        point = 0
+        for word in report_str.split():
+            if word in positive_feedback:
+                point += 3
+            elif word in negative_feedback:
+                point -= 1
+        points[s_id] = point
+        '''
+        # One-liner:
+        points[s_id] = sum(3*(word in positive_feedback)-1*(word in negative_feedback) for word in report_str.split())
+    # print(points)  #debugging
+
+    # print(sorted(points, key=lambda id_: (-points[id_], id_)))  #debugging
+    return sorted(points, key=lambda id_: (-points[id_], id_))[:k]
+    """
+
+    # 1.2) Turns out that we don't need dict:
 
     positive_feedback, negative_feedback = set(positive_feedback), set(negative_feedback)  # allow fast lookup
 
     points = []
-    stud_id = {}
-    count = 0
-
-    for r, s_id in zip(report, student_id):
-
+    for report_str, s_id in zip(report, student_id):
+        '''
         point = 0
-        for w in r.split():
-            if w in positive_feedback:
+        for word in report_str.split():
+            if word in positive_feedback:
                 point += 3
-            elif w in negative_feedback:
+            elif word in negative_feedback:
                 point -= 1
-        
-        heappush(points, point)
-        count += 1
-        if count > k:
-            heappop(points)
-        
-        stud_id[point] = chain(stud_id.get(point, []), [s_id])
-    
-    # print(points, stud_id)
-    
-    ans = []
-    last = None
-    
-    while points:
-        p = heappop(points)
-        if p != last:
-            ans.extend(sorted(stud_id[p], reverse=True))
-            last = p
-    
-    return ans[::-1][:k]
+        points.append((-point, s_id))
+        '''
+        # One-liner:
+        points.append((-sum(3*(word in positive_feedback)-1*(word in negative_feedback) for word in report_str.split()),
+                       s_id))
+    # print(points)  #debugging
+
+    # print([s_id for _, s_id in sorted(points)])  #debugging
+    return [s_id for _, s_id in sorted(points)[:k]]
--- Contests/Biweekly Contest 94/3) Minimize the Maximum of Two Arrays.py
+++ Contests/Biweekly Contest 94/3) Minimize the Maximum of Two Arrays.py
@@ -3,21 +3,39 @@ https://leetcode.com/problems/minimize-the-maximum-of-two-arrays
 """
 
 
-def minimize_set(divisor1: int, divisor2: int, unique_cnt1: int, unique_cnt2: int) -> int:
+def minimize_set(d1: int, d2: int, c1: int, c2: int) -> int:
     """"""
 
-    if divisor1 > divisor2:
-        return minimize_set(divisor2, divisor1, unique_cnt2, unique_cnt1)
-    
-    n = 0
-    
-    while unique_cnt1 != 0 or unique_cnt2 != 0:
-        
-        n += 1
-        if unique_cnt1 > 0 and n % divisor1:
-            unique_cnt1 -= 1
-            continue
-        if unique_cnt2 > 0 and n % divisor2:
-            unique_cnt2 -= 1
-    
-    return n
+    # Use binary search to find the smallest maximum element.
+    # Add numbers divisible by x in nums2 and vice versa.
+
+    # 1) Sub-Optimal (Binary Search + Maths): TC = O(O(log(min(d1, d2))) + log(2^31 - 1)); SC = O(1)
+    # https://leetcode.com/problems/minimize-the-maximum-of-two-arrays/solutions/2946508/python-lcm-and-binary-search-explained-bonus-one-liner
+    # https://leetcode.com/problems/minimize-the-maximum-of-two-arrays/solutions/2946511/binary-search-venn-diagram-set-diagrams-explanation-with-diagram
+    # https://leetcode.com/problems/minimize-the-maximum-of-two-arrays/solutions/2947016/binary-search-on-answer-intuition-approach-explained-c
+
+    from math import lcm
+    # Definition: https://github.com/samyak1409/python-lab-assignments/blob/main/9/b.py
+
+    lt, rt = 2, 2147483647  # `lt = 2` since "2 <= uniqueCnt1 + uniqueCnt2"
+    # `rt = 2147483647` i.e. 2^31 - 1 i.e. max int (32-bit) (https://en.wikipedia.org/wiki/2,147,483,647#In_computing)
+    # Python doesn't have int limit, but other langs have, so.
+    d1d2 = lcm(d1, d2)  # TC = O(log(min(d1, d2)))
+    # https://www.geeksforgeeks.org/time-complexity-of-euclidean-algorithm
+    # https://www.baeldung.com/cs/euclid-time-complexity
+
+    while lt < rt:
+        val = (lt+rt) // 2
+        if val-(val//d1) >= c1 and val-(val//d2) >= c2 and val-(val//d1d2) >= c1+c2:  # => `val` could be the ans
+            rt = val  # compressing the search range: let's see if a smaller `val` satisfies the conditions
+        else:  # => we need a bigger `val` in order to fill the arrays with the required conditions
+            lt = val + 1  # compressing the search range: `val` can't be the ans, so `lt = val + 1`
+    return rt  # or lt
+
+    # Explanation of `if val-(val//d1) >= c1 and val-(val//d2) >= c2 and val-(val//d1d2) >= c1+c2`:
+    # `val-(val//d1)   >= c1`   : ([1, val] - multiples of d1 in               [1, val]) >= c1
+    # `val-(val//d2)   >= c2`   : ([1, val] - multiples of d2 in               [1, val]) >= c2
+    # `val-(val//d1d2) >= c1+c2`: ([1, val] - multiples of d1 as well as d2 in [1, val]) >= c1+c2
+
+    # 2) Optimal (Maths): TC = O(log(min(d1, d2))); SC = O(1)
+    # https://leetcode.com/problems/minimize-the-maximum-of-two-arrays/solutions/2947014/formula
