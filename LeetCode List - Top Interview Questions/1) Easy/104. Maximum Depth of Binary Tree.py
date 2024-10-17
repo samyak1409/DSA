afd482e66ba08c8acef3300f4dbb2e39cb8e78b3
Samyak Jain <samyak65400@gmail.com>
2024-10-17 12:56:56 +0530

    update solutions in TIQ list: 104

--- LeetCode List - Top Interview Questions/1) Easy/104. Maximum Depth of Binary Tree.py
+++ LeetCode List - Top Interview Questions/1) Easy/104. Maximum Depth of Binary Tree.py
@@ -12,33 +12,54 @@ class TreeNode:
 
 
 def max_depth(root: TreeNode | None) -> int:
+    """"""
 
-    # Iterative:
-    # TC = O(n) (we are just going through each and every node in the tree one time)
-    # SC = O(n) (last level of the binary tree have (n+1)/2 nodes at most)
+    # 1) Optimal (Iterative: Level order traversal, +1 count on each level):
+    # TC = O(n) (going through every node)
+    # SC = O(n) (last level of binary tree have (n//2 + 1) nodes at most)
 
     """
-    queue = [root]
-    height = -1
+    level = [root]
+    depth = -1
 
-    while queue:  # will loop h times, h: height of binary tree
+    while level:  # will loop h times, h: height of binary tree
 
-        height += 1
+        depth += 1
 
-        queue_copy = queue.copy()  # "queue.copy()" / "queue[:]" -> new copy ✅
-
-        queue.clear()
-
-        for node in queue_copy:  # will loop 1, 2, 4, 8, ... (n+1)/2 times
+        level_copy = level.copy()
+        level.clear()
+        for node in level_copy:  # will loop 1, 2, 4, 8, ... (n//2 + 1) times
             if node:
-                queue.extend([node.left, node.right])
+                level.extend([node.left, node.right])
 
-    return height
+    return depth
     """
 
-    # Recursive: TC = O(n); SC = O(h) (height of the binary tree) = O(n) in worst case and O(log n) in best case
+    # 2) Optimal (Recursive): TC = O(n); SC = O(h) {avg. case: O(log(n)); worst case: O(n)}
+    # Recurse on each node left and right, and +1 each time.
 
-    if not root:  # base condition
+    # 2.1) Intuitive:
+    """
+    # Recursive Function:
+    def recurse(node: TreeNode | None , curr_depth: int) -> None:
+        if node is None:  # base case of recursion: if node is None, return
+            return
+        # Else, update the ans:
+        ans[0] = max(ans[0], curr_depth)
+        # And then recurse further in:
+        recurse(node.left, curr_depth+1), recurse(node.right, curr_depth+1)
+
+    ans = [0]  # list for mutability
+    recurse(root, 1)
+    return ans[0]
+    """
+
+    # 2.2) "Pro recursion" / "Power of recursion":
+    # https://leetcode.com/problems/maximum-depth-of-binary-tree/solutions
+
+    if not root:  # base case
         return 0
-
+    # Just return the max of max_depths of left and right subtree, and recursion will solve the sub-subtrees:
     return max(max_depth(root.left), max_depth(root.right)) + 1
+
+    # (While submitting on LC: `max_depth` -> `self.maxDepth`)
