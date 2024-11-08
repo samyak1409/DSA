2c072039ee12c9c64deaf9dbbad7dee6cbc73677
Samyak Jain <samyak65400@gmail.com>
2024-11-08 11:14:39 +0530

    update solutions in TIQ list: 242

--- LeetCode List - Top Interview Questions/1) Easy/242. Valid Anagram.py
+++ LeetCode List - Top Interview Questions/1) Easy/242. Valid Anagram.py
@@ -4,52 +4,62 @@ https://leetcode.com/problems/valid-anagram
 
 
 def is_anagram(s: str, t: str) -> bool:
+    """"""
 
-    # 0) Minor Optimization for every method:
+    # 0) Brute-force (Sort): TC = O(n*log(n)); SC = O(n)
+
+    """
+    # Small optimization:
+    if len(s) != len(t):  # O(1)
+        return False
+
+    return sorted(s) == sorted(t)
+    """
+
+    # 0.1) Optimized Brute-force (Checking ord sum before Sort):
+    # Avg. Case: TC = O(n); SC = O(1)
+    # Worst Case: TC = O(n*log(n)); SC = O(n) (e.g. s = "abc", t = "bbb")
+
+    """
+    if len(s) != len(t):  # O(1)
+        return False
+
+    # Added optimization on `0)`:
+    return sum(map(ord, s)) == sum(map(ord, t)) and sorted(s) == sorted(t)
+    """
+
+    # 1) Optimal (HashMap): TC = O(n); SC = O(26) ("s and t consist of lowercase English letters")
+
+    """
+    if len(s) != len(t):  # O(1)
+        return False
+
+    hm1 = {}
+    for char in s:
+        hm1[char] = hm1.get(char, 0) + 1
+    hm2 = {}
+    for char in t:
+        hm2[char] = hm2.get(char, 0) + 1
+    return hm1 == hm2
+    """
+    # Pythonic:
+    """
+    from collections import Counter
 
     if len(s) != len(t):  # O(1)
         return False
 
-    # 1) Naive (Using Sorting): TC = O(n log n); SC = O(n)
-
-    """
-    return sorted(s) == sorted(t)
+    return Counter(s) == Counter(t)
     """
 
-    # 1.1) Better:
-    # Average Case: TC = O(n); SC = O(1);
-    # Worst Case: TC = O(n log n); SC = O(n) (e.g. when s = "abc", t = "bbb")
+    # 1.1) Better: Array as HashMap:
 
-    """
-    return sum(map(ord, s)) == sum(map(ord, t)) and sorted(s) == sorted(t)
-    """
+    if len(s) != len(t):  # O(1)
+        return False
 
-    # 3) https://leetcode.com/problems/valid-anagram/discuss/66484/Accepted-Java-O(n)-solution-in-5-lines:
-    # TC = O(n); SC = O(1)
-
-    # Note- Q. constraint 2: "s and t consist of lowercase English letters"
-
-    """
     arr = [0] * 26
     for c in s:
         arr[ord(c)-97] += 1
     for c in t:
         arr[ord(c)-97] -= 1
     return arr == [0] * 26
-    """
-
-    # 2) Using Hash Table:
-    # TC = O(n); SC = O(1) (because of Q. constraint 2: "s and t consist of lowercase English letters")
-
-    """
-    ht1 = {}
-    for char in s:
-        ht1[char] = ht1.get(char, 0) + 1
-    ht2 = {}
-    for char in t:
-        ht2[char] = ht2.get(char, 0) + 1
-    return ht1 == ht2
-    """
-
-    from collections import Counter
-    return Counter(s) == Counter(t)
