da059f224488a752b0656b5b6fc06ec29d3ec4d4
Samyak Jain <samyak65400@gmail.com>
2024-11-26 21:52:15 +0530

    update solutions in TIQ list: 344

--- LeetCode List - Top Interview Questions/1) Easy/344. Reverse String.py
+++ LeetCode List - Top Interview Questions/1) Easy/344. Reverse String.py
@@ -8,7 +8,53 @@ def reverse_string(s: list[str]) -> None:
     Do not return anything, modify s in-place instead.
     """
 
-    # Two Pointer: TC = O(n); SC = O(1)
+    # 1) Optimal (Two Pointers): TC = O(n); SC = O(1)
 
+    # 1.2) Using while loop:
+    # https://leetcode.com/problems/reverse-string/solutions/3718521/easy-solution-with-simple-explanation
+    """
+    i, j = 0, len(s)-1
+    while i < j:
+        s[i], s[j] = s[j], s[i]
+        i, j = i+1, j-1
+    """
+
+    # 1.1) Using for loop:
+    # https://leetcode.com/problems/reverse-string/solutions/669571/python-oneliner-two-pointers-explained
+    """
     for i in range(len(s)//2):
-        s[i], s[-i-1] = s[-i-1], s[i]
+        s[i], s[~i] = s[~i], s[i]  # (~i = -i-1)
+    """
+
+    # 2) Time-Optimal (Recursion): TC = O(n); SC = O(n) {recursion stack}
+    # """
+    # interviewer: "implement your own function, how about recursion"
+    # *sweats*
+    # This problem is a litmus test of whether a programmer will be replaced by chatgpt. This is not a test of our
+    # syntax.
+    # """
+    # -https://leetcode.com/problems/reverse-string/description/comments/2022019
+
+    # 2.1) Swap while recursing out:
+    """
+    # Recursive Function:
+    def reverse(i: int = 0) -> None:
+        if i != n-1:
+            reverse(i=i+1)  # recurse in
+        # While recursing out:
+        if i >= n//2:
+            s[i], s[~i] = s[~i], s[i]
+
+    n = len(s)
+    reverse()
+    """
+
+    # 2.2) Swap while recursing in:
+    # Recursive Function:
+    def reverse(i: int = 0) -> None:
+        if i < n//2:
+            s[i], s[~i] = s[~i], s[i]
+            reverse(i=i+1)  # recurse in
+
+    n = len(s)
+    reverse()
