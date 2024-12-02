c104d6ef9e9514a8dd2625510964f8e7f75e26ef
Samyak Jain <samyak65400@gmail.com>
2024-12-03 02:02:04 +0530

    update solutions in TIQ list: 387

--- LeetCode List - Top Interview Questions/1) Easy/202. Happy Number.py
+++ LeetCode List - Top Interview Questions/1) Easy/202. Happy Number.py
@@ -1,3 +1,3 @@
 """
-https://github.com/samyak1409/DSA/blob/d7fde4442632ac3783c6bfde2e20148e515126c2/SDE%20Sheet/02%29%20Linked%20List/032.1%29%20Happy%20Number.py
+https://github.com/samyak1409/DSA/blob/main/SDE%20Sheet/02%29%20Linked%20List/032.1%29%20Happy%20Number.py
 """
--- LeetCode List - Top Interview Questions/1) Easy/387. First Unique Character in a String.py
+++ LeetCode List - Top Interview Questions/1) Easy/387. First Unique Character in a String.py
@@ -4,39 +4,45 @@ https://leetcode.com/problems/first-unique-character-in-a-string
 
 
 def first_uniq_char(s: str) -> int:
+    """"""
 
-    # 1) Brute Force: TC = O(n^2); SC = O(1)
+    # 0) [TLE] Brute-force (Loop and Count): TC = O(n^2); SC = O(1)
+    # Loop, count occurrence, return first char with count = 1.
 
     """
-    for i, c in enumerate(s):
-        if s.count(c) == 1:
+    for i, c in enumerate(s):  # O(n)
+        if s.count(c) == 1:  # O(n)
             return i
-
     return -1
     """
 
-    # 2) Using Hash Table:
-    # TC = O(n); SC = O(1) (because of Q. constraint 2: "s consists of only lowercase English letters")
+    # 1) Optimal (Loop, Count, Save to HashMap): TC = O(26*n) = O(n); SC = O(26) = O(1)
+    # Optimization of approach `0)` using hashmap.
 
     """
+    hm = {}
+    for i, c in enumerate(s):  # O(n)
+        if hm.get(c) is None:
+            hm[c] = s.count(c)  # O(n), but because of the above `if` condition, this line would run max 26 times
+        if hm[c] == 1:
+            return i
+    return -1
+    """
+
+    # 2) Optimal (HashMap): TC = O(n); SC = O(26) = O(1)
+    # Make a hashmap first, and then return the first char with occurrences = 1.
+
     from collections import Counter
-    ht = Counter(s)
+    hm = Counter(s)  # O(n); O(26)
 
-    for i, c in enumerate(s):
-        if ht[c] == 1:
-            return i
-    return -1    
-    """
+    # If `Counter` didn't preserve insertion order, then we would've done:
+    # for i, c in enumerate(s):  # O(n)
+    #     if hm[c] == 1:
+    #         return i
+    # return -1
 
-    ht = {}  # key = chr, value = 1 if chr is non-repeating else 0
-    for c in s:
-        if c in ht:  # O(1) coz ht will have 26 items at max
-            ht[c] = 0
-        else:
-            ht[c] = 1
-    # print(ht)  #debugging
-
-    for i, c in enumerate(s):
-        if ht[c]:
-            return i
+    # But as it does (just like `dict`), we can also do:
+    for c, x in hm.items():  # O(n)
+        if x == 1:
+            return s.index(c)
     return -1
