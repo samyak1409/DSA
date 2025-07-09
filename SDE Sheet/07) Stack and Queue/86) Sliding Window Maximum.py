8b3e9b4553e625c1514e1761e4bb981d213ab7da
Samyak Jain <samyak65400@gmail.com>
2025-07-09 18:24:49 +0530

    monotype OA

---
+++ SDE Sheet/07) Stack and Queue/86) Sliding Window Maximum.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/sliding-window-maximum
+"""
+
+
+def max_sliding_window(nums: list[int], k: int) -> list[int]:
+    """"""
+
+    # Came in my Monotype SE-1 (~10LPA) OA, time limit 25 mins (failed to do):
+    # https://chatgpt.com/share/686e65a4-648c-800a-87dd-8622416b58c4
+    # https://g.co/gemini/share/55083c997ded
+
+    # 1) Optimal (Monotonic Decreasing Queue): TC = O(n); SC = O(n)
+    # Not hard.
+
+    pass
